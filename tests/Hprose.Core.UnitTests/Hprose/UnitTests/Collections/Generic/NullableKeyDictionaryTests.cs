e5d4c5f2592ce063438ef7a9767baea5bbce5244
andot <mabingyao@gmail.com>
2018-03-24 01:39:47 +0800

    Added Hprose.Core.UnitTests

---
+++ tests/Hprose.Core.UnitTests/Hprose/UnitTests/Collections/Generic/NullableKeyDictionaryTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Hprose.Collections.Generic;
+using System.Linq;
+
+namespace Hprose.Core.UnitTests.Hprose.UnitTests.Collections.Generic {
+    [TestClass]
+    public class NullableKeyDictionaryTests {
+        [TestMethod]
+        public void TestNullableKeyDictionaryString() {
+            var dict = new NullableKeyDictionary<string, string> {
+                { "Hello", "World" },
+                { null, "Null" }
+            };
+            Assert.AreEqual(dict.Count, 2);
+        }
+        [TestMethod]
+        public void TestNullableKeyDictionaryInt() {
+            var dict = new NullableKeyDictionary<int?, string> {
+                { 1, "Hello" },
+                { null, "Null" }
+            };
+            Assert.AreEqual(dict.Count, 2);
+        }
+        [TestMethod]
+        public void TestNullableKeyDictionaryKeys() {
+            var dict = new NullableKeyDictionary<string, string> {
+                { "Hello", "World" },
+                { null, "Null" }
+            };
+            var keys = dict.Keys;
+            var keys1 = new String[2];
+            keys.CopyTo(keys1, 0);
+            var keys2 = new String[] { "Hello", null };
+            Assert.IsTrue(keys1.SequenceEqual(keys2));
+        }
+        [TestMethod]
+        public void TestNullableKeyDictionaryContainsKey() {
+            var dict = new NullableKeyDictionary<string, string>(StringComparer.OrdinalIgnoreCase) {
+                { "Hello", "World" },
+                { null, "Null" }
+            };
+            Assert.AreEqual(dict["hello"], dict["Hello"]);
+            Assert.IsTrue(dict.ContainsKey("hello"));
+            Assert.IsTrue(dict.ContainsKey(null));
+        }
+    }
+}
---
+++ tests/Hprose.Core.UnitTests/Hprose/UnitTests/Collections/Generic/NullableKeyTests.cs
@@ -0,0 +1,48 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Hprose.Collections.Generic;
+using System.Collections.Generic;
+
+namespace Hprose.UnitTests.Collections.Generic {
+    [TestClass]
+    public class NullableKeyTests {
+        [TestMethod]
+        public void TestNullableKeyString() {
+            var dict = new Dictionary<NullableKey<string>, string> {
+                { "Hello", "World" },
+                { null, "Null" }
+            };
+            Assert.AreEqual<int>(dict.Count, 2);
+        }
+        [TestMethod]
+        public void TestNullableKeyInt() {
+            var dict = new Dictionary<NullableKey<int?>, string> {
+                { 1, "Hello" },
+                { null, "Null" }
+            };
+            Assert.AreEqual<int>(dict.Count, 2);
+        }
+        [TestMethod]
+        public void TestEquals() {
+            NullableKey<String> s = "hello";
+            Assert.IsTrue(s == "hello");
+            s = null;
+            Assert.IsTrue(s == null);
+            Assert.IsFalse(s.Equals(0));
+        }
+        [TestMethod]
+        public void TestGetHashCode() {
+            NullableKey<String> s = "hello";
+            Assert.IsTrue(s.GetHashCode() == "hello".GetHashCode());
+            s = null;
+            Assert.IsTrue(s.GetHashCode() == 0);
+        }
+        [TestMethod]
+        public void TestCompareTo() {
+            NullableKey<String> s = "hello";
+            Assert.IsTrue(s.CompareTo("hello") == 0);
+            s = null;
+            Assert.IsTrue(s.CompareTo("hello") < 0);
+        }
+    }
+}
