be6c5d5719a3b1f41fb3d8c9a6824e70b140b880
andot <mabingyao@gmail.com>
2018-03-29 12:26:37 +0800

    Added ReferenceSerializer.cs

---
+++ src/Hprose.IO/Serializers/ReferenceSerializer.cs
@@ -0,0 +1,33 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * ReferenceSerializer.cs                                 *
+ *                                                        *
+ * hprose ReferenceSerializer class for C#.               *
+ *                                                        *
+ * LastModified: Mar 29, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose.IO.Serializers {
+    public abstract class ReferenceSerializer<T> : Serializer<T> {
+
+        // write your actual serialization code in sub class
+        public virtual void Serialize(Writer writer, T obj) => writer.SetRef(obj);
+
+        public override void Write(Writer writer, T obj) {
+            if (!writer.WriteRef(obj)) {
+                Serialize(writer, obj);
+            }
+        }
+
+    }
+}
