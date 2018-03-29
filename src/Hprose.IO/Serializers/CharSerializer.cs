2d1e77560fda7ed7ac247d32bfe4953a9516509d
andot <mabingyao@gmail.com>
2018-03-29 12:21:41 +0800

    Added CharSerializer.cs

---
+++ src/Hprose.IO/Serializers/CharSerializer.cs
@@ -0,0 +1,26 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * CharSerializer.cs                                      *
+ *                                                        *
+ * CharSerializer class for C#.                           *
+ *                                                        *
+ * LastModified: Mar 28, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose.IO.Serializers {
+    class CharSerializer : Serializer<char> {
+        private static readonly CharSerializer instance = new CharSerializer();
+        public static CharSerializer Instance => instance;
+        public override void Write(Writer writer, char obj) => ValueWriter.Write(writer.Stream, obj);
+    }
+}
