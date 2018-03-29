5f9aecd7f719138575813e23ac2de39432f00479
andot <mabingyao@gmail.com>
2018-03-29 12:22:48 +0800

    Added Int16Serializer.cs

---
+++ src/Hprose.IO/Serializers/Int16Serializer.cs
@@ -0,0 +1,26 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * Int16Serializer.cs                                     *
+ *                                                        *
+ * Int16Serializer class for C#.                          *
+ *                                                        *
+ * LastModified: Mar 29, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose.IO.Serializers {
+    class Int16Serializer : Serializer<short> {
+        private static readonly Int16Serializer _instance = new Int16Serializer();
+        public static Int16Serializer Instance => _instance;
+        public override void Write(Writer writer, short obj) => ValueWriter.Write(writer.Stream, obj);
+    }
+}
