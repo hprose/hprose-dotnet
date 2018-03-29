dbe09536ab492038ada15cee98f13488ee12627b
andot <mabingyao@gmail.com>
2018-03-29 12:17:28 +0800

    Added HproseMode.cs

---
+++ src/Hprose.IO/HproseMode.cs
@@ -0,0 +1,24 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * HproseMode.cs                                          *
+ *                                                        *
+ * hprose mode enum for C#.                               *
+ *                                                        *
+ * LastModified: Mar 28, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose.IO {
+    public enum HproseMode {
+        FieldMode, PropertyMode, MemberMode
+    }
+}
