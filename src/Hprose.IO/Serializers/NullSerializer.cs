3822254deb5b0866157868dbc08006db416f897e
andot <mabingyao@gmail.com>
2018-03-29 12:20:46 +0800

    Added NullSerializer.cs

---
+++ src/Hprose.IO/Serializers/NullSerializer.cs
@@ -0,0 +1,26 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * NullSerializer.cs                                      *
+ *                                                        *
+ * NullSerializer class for C#.                           *
+ *                                                        *
+ * LastModified: Mar 28, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose.IO.Serializers {
+    class NullSerializer : Serializer {
+        private static readonly NullSerializer _instance = new NullSerializer();
+        public static NullSerializer Instance => _instance;
+        public override void Write(Writer writer, object obj) => writer.Stream.WriteByte(HproseTags.TagNull);
+    }
+}
