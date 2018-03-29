64e3f53574acca5581c24e43bb441cd07719a911
andot <mabingyao@gmail.com>
2018-03-29 12:24:07 +0800

    Added SingleSerializer.cs

---
+++ src/Hprose.IO/Serializers/SingleSerializer.cs
@@ -0,0 +1,26 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * SingleSerializer.cs                                    *
+ *                                                        *
+ * SingleSerializer class for C#.                         *
+ *                                                        *
+ * LastModified: Mar 29, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose.IO.Serializers {
+    class SingleSerializer : Serializer<float> {
+        private static readonly SingleSerializer _instance = new SingleSerializer();
+        public static SingleSerializer Instance => _instance;
+        public override void Write(Writer writer, float obj) => ValueWriter.Write(writer.Stream, obj);
+    }
+}
