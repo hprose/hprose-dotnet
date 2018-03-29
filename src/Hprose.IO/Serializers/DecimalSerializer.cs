c4246fcdbcafff0e9ad60a4b9da2fa078e3602e8
andot <mabingyao@gmail.com>
2018-03-29 12:24:33 +0800

    Added DecimalSerializer.cs

---
+++ src/Hprose.IO/Serializers/DecimalSerializer.cs
@@ -0,0 +1,26 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * DecimalSerializer.cs                                   *
+ *                                                        *
+ * DecimalSerializer class for C#.                        *
+ *                                                        *
+ * LastModified: Mar 29, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose.IO.Serializers {
+    class DecimalSerializer : Serializer<decimal> {
+        private static readonly DecimalSerializer _instance = new DecimalSerializer();
+        public static DecimalSerializer Instance => _instance;
+        public override void Write(Writer writer, decimal obj) => ValueWriter.Write(writer.Stream, obj);
+    }
+}
