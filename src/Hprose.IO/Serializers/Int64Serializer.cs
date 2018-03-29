caba31894e03d9517e2638749ee4b5526b90108a
andot <mabingyao@gmail.com>
2018-03-29 12:23:37 +0800

    Added Int64Serializer.cs

---
+++ src/Hprose.IO/Serializers/Int64Serializer.cs
@@ -0,0 +1,26 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * Int64Serializer.cs                                     *
+ *                                                        *
+ * Int64Serializer class for C#.                          *
+ *                                                        *
+ * LastModified: Mar 29, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose.IO.Serializers {
+    class Int64Serializer : Serializer<long> {
+        private static readonly Int64Serializer _instance = new Int64Serializer();
+        public static Int64Serializer Instance => _instance;
+        public override void Write(Writer writer, long obj) => ValueWriter.Write(writer.Stream, obj);
+    }
+}
