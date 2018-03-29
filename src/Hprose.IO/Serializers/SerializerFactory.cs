ff2e5148d4f4d06088ccc25419965fab7773c2b8
andot <mabingyao@gmail.com>
2018-03-29 16:36:39 +0800

    Added UIntPtrSerializer

--- src/Hprose.IO/Serializers/SerializerFactory.cs
+++ src/Hprose.IO/Serializers/SerializerFactory.cs
@@ -41,6 +41,7 @@ namespace Hprose.IO.Serializers {
             _serializers[typeof(double)] = DoubleSerializer.Instance;
             _serializers[typeof(decimal)] = DecimalSerializer.Instance;
             _serializers[typeof(IntPtr)] = IntPtrSerializer.Instance;
+            _serializers[typeof(UIntPtr)] = UIntPtrSerializer.Instance;
             _serializers[typeof(BigInteger)] = BigIntegerSerializer.Instance;
             _serializers[typeof(Guid)] = GuidSerializer.Instance;
             _serializers[typeof(TimeSpan)] = TimeSpanSerializer.Instance;
@@ -59,6 +60,7 @@ namespace Hprose.IO.Serializers {
             _serializers[typeof(double?)] = NullableSerializer<double>.Instance;
             _serializers[typeof(decimal?)] = NullableSerializer<decimal>.Instance;
             _serializers[typeof(IntPtr?)] = NullableSerializer<IntPtr>.Instance;
+            _serializers[typeof(UIntPtr?)] = NullableSerializer<UIntPtr>.Instance;
             _serializers[typeof(BigInteger?)] = NullableSerializer<BigInteger>.Instance;
             _serializers[typeof(Guid?)] = NullableSerializer<Guid>.Instance;
             _serializers[typeof(TimeSpan?)] = NullableSerializer<TimeSpan>.Instance;
@@ -77,6 +79,7 @@ namespace Hprose.IO.Serializers {
             _serializers[typeof(NullableKey<double?>)] = NullableKeySerializer<double?>.Instance;
             _serializers[typeof(NullableKey<decimal?>)] = NullableKeySerializer<decimal?>.Instance;
             _serializers[typeof(NullableKey<IntPtr?>)] = NullableKeySerializer<IntPtr?>.Instance;
+            _serializers[typeof(NullableKey<UIntPtr?>)] = NullableKeySerializer<UIntPtr?>.Instance;
             _serializers[typeof(NullableKey<BigInteger?>)] = NullableKeySerializer<BigInteger?>.Instance;
             _serializers[typeof(NullableKey<Guid?>)] = NullableKeySerializer<Guid?>.Instance;
             _serializers[typeof(NullableKey<TimeSpan?>)] = NullableKeySerializer<TimeSpan?>.Instance;
---
+++ src/Hprose.IO/Serializers/UIntPtrSerializer.cs
@@ -0,0 +1,27 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * UIntPtrSerializer.cs                                   *
+ *                                                        *
+ * UIntPtrSerializer class for C#.                        *
+ *                                                        *
+ * LastModified: Mar 29, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+using System;
+
+namespace Hprose.IO.Serializers {
+    class UIntPtrSerializer : Serializer<UIntPtr> {
+        private static readonly UIntPtrSerializer _instance = new UIntPtrSerializer();
+        public static UIntPtrSerializer Instance => _instance;
+        public override void Write(Writer writer, UIntPtr obj) => ValueWriter.Write(writer.Stream, obj);
+    }
+}
