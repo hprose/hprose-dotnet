88efff547f28828878d7a0bf9649b78fe916a484
andot <mabingyao@gmail.com>
2018-03-29 12:20:24 +0800

    Added Serializer.cs

---
+++ src/Hprose.IO/Serializers/Serializer.cs
@@ -0,0 +1,28 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * Serializer.cs                                          *
+ *                                                        *
+ * hprose Serializer class for C#.                        *
+ *                                                        *
+ * LastModified: Mar 28, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose.IO.Serializers {
+    public abstract class Serializer {
+        public abstract void Write(Writer writer, object obj);
+    }
+    public abstract class Serializer<T> : Serializer {
+        public abstract void Write(Writer writer, T obj);
+        public override void Write(Writer writer, object obj) => Write(writer, (T)obj);
+    }
+}
