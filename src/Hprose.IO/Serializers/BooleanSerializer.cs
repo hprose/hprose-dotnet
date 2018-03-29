e56353f3760d8e24b0bfd8386a085c8e2bcb09e0
andot <mabingyao@gmail.com>
2018-03-29 12:21:21 +0800

    Added BooleanSerializer.cs

---
+++ src/Hprose.IO/Serializers/BooleanSerializer.cs
@@ -0,0 +1,26 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * BooleanSerializer.cs                                   *
+ *                                                        *
+ * BooleanSerializer class for C#.                        *
+ *                                                        *
+ * LastModified: Mar 28, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose.IO.Serializers {
+    class BooleanSerializer : Serializer<bool> {
+        private static readonly BooleanSerializer _instance = new BooleanSerializer();
+        public static BooleanSerializer Instance => _instance;
+        public override void Write(Writer writer, bool obj) => ValueWriter.Write(writer.Stream, obj);
+    }
+}
