abea3b1e24a197074f7b100c63a5aa0d0a7876e9
andot <mabingyao@gmail.com>
2018-03-29 12:21:02 +0800

    Added ByteSerializer.cs

---
+++ src/Hprose.IO/Serializers/ByteSerializer.cs
@@ -0,0 +1,26 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * ByteSerializer.cs                                      *
+ *                                                        *
+ * ByteSerializer class for C#.                           *
+ *                                                        *
+ * LastModified: Mar 28, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose.IO.Serializers {
+    class ByteSerializer : Serializer<byte> {
+        private static readonly ByteSerializer _instance = new ByteSerializer();
+        public static ByteSerializer Instance => _instance;
+        public override void Write(Writer writer, byte obj) => ValueWriter.Write(writer.Stream, obj);
+    }
+}
