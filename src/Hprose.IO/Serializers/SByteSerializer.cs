b25dcc6fb55ef2f1f594e26008c1bc11e4091735
andot <mabingyao@gmail.com>
2018-03-29 12:22:16 +0800

    Added SByteSerializer.cs

---
+++ src/Hprose.IO/Serializers/SByteSerializer.cs
@@ -0,0 +1,26 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * SByteSerializer.cs                                     *
+ *                                                        *
+ * SByteSerializer class for C#.                          *
+ *                                                        *
+ * LastModified: Mar 28, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose.IO.Serializers {
+    class SByteSerializer : Serializer<sbyte> {
+        private static readonly SByteSerializer _instance = new SByteSerializer();
+        public static SByteSerializer Instance => _instance;
+        public override void Write(Writer writer, sbyte obj) => ValueWriter.Write(writer.Stream, obj);
+    }
+}
