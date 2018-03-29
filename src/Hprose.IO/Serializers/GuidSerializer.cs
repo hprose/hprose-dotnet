6e61e7583723efc847916bf90eaf064ab12547f8
andot <mabingyao@gmail.com>
2018-03-29 12:26:48 +0800

    Added GuidSerializer.cs

---
+++ src/Hprose.IO/Serializers/GuidSerializer.cs
@@ -0,0 +1,37 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * GuidSerializer.cs                                      *
+ *                                                        *
+ * GuidSerializer class for C#.                           *
+ *                                                        *
+ * LastModified: Mar 29, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+using System;
+using System.IO;
+using static Hprose.IO.HproseTags;
+
+namespace Hprose.IO.Serializers {
+    class GuidSerializer : ReferenceSerializer<Guid> {
+        private static readonly GuidSerializer _instance = new GuidSerializer();
+        public static GuidSerializer Instance => _instance;
+        public override void Serialize(Writer writer, Guid obj) {
+            base.Serialize(writer, obj);
+            Stream stream = writer.Stream;
+            stream.WriteByte(TagGuid);
+            stream.WriteByte(TagOpenbrace);
+            byte[] buf = ValueWriter.GetASCII(obj.ToString());
+            stream.Write(buf, 0, buf.Length);
+            stream.WriteByte(TagClosebrace);
+        }
+    }
+}
