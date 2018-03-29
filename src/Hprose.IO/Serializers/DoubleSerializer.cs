48328156728e9251fdb1805e79c998fe2d5d9536
andot <mabingyao@gmail.com>
2018-03-29 12:24:18 +0800

    Added DoubleSerializer.cs

---
+++ src/Hprose.IO/Serializers/DoubleSerializer.cs
@@ -0,0 +1,26 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * DoubleSerializer.cs                                    *
+ *                                                        *
+ * DoubleSerializer class for C#.                         *
+ *                                                        *
+ * LastModified: Mar 29, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose.IO.Serializers {
+    class DoubleSerializer : Serializer<double> {
+        private static readonly DoubleSerializer _instance = new DoubleSerializer();
+        public static DoubleSerializer Instance => _instance;
+        public override void Write(Writer writer, double obj) => ValueWriter.Write(writer.Stream, obj);
+    }
+}
