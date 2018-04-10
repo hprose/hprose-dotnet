65ce5ae4d0d617804a663728b2138b3a8fa2421a
andot <mabingyao@gmail.com>
2018-04-10 13:30:52 +0800

    Added UInt64Deserializer

---
+++ src/Hprose.IO/Deserializers/UInt64Deserializer.cs
@@ -0,0 +1,52 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * UInt64Deserializer.cs                                  *
+ *                                                        *
+ * UInt64Deserializer class for C#.                       *
+ *                                                        *
+ * LastModified: Apr 9, 2018                              *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+using Hprose.IO.Converters;
+
+using static Hprose.IO.HproseTags;
+
+namespace Hprose.IO.Deserializers {
+    class UInt64Deserializer : Deserializer<ulong> {
+        public override ulong Read(Reader reader, int tag) {
+            if (tag >= '0' && tag <= '9') {
+                return (ulong)(tag - '0');
+            }
+            var stream = reader.Stream;
+            switch (tag) {
+                case TagInteger:
+                    return (ulong)ValueReader.ReadInt(stream);
+                case TagLong:
+                    return (ulong)ValueReader.ReadLong(stream);
+                case TagDouble:
+                    return (ulong)ValueReader.ReadDouble(stream);
+                case TagTrue:
+                    return 1;
+                case TagFalse:
+                case TagEmpty:
+                    return 0;
+                case TagUTF8Char:
+                    return Converter<ulong>.Instance.Convert(ValueReader.ReadUTF8Char(stream));
+                case TagString:
+                    return Converter<ulong>.Instance.Convert(ReferenceReader.ReadString(reader));
+                default:
+                    return base.Read(reader, tag);
+            }
+        }
+    }
+}
