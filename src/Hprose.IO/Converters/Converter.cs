2b51f7c202366b7595f7edf2dd987695e1d33191
andot <mabingyao@gmail.com>
2018-04-10 13:23:09 +0800

    Added Converter.cs

---
+++ src/Hprose.IO/Converters/Converter.cs
@@ -0,0 +1,84 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * Converter.cs                                           *
+ *                                                        *
+ * hprose Converter class for C#.                         *
+ *                                                        *
+ * LastModified: Apr 10, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+using System;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Numerics;
+
+namespace Hprose.IO.Converters {
+    public interface IConverter { }
+    public class Converter<T> : IConverter {
+        static Converter() => Converter.Initialize();
+        private static volatile Converter<T> _instance;
+        public static Converter<T> Instance {
+            get {
+                if (_instance == null) {
+                    _instance = Converter.GetInstance(typeof(T)) as Converter<T>;
+                }
+                return _instance;
+            }
+        }
+        private static TypeConverter typeConverter = TypeDescriptor.GetConverter(typeof(T));
+        public virtual T Convert(object obj) {
+            return (T)typeConverter.ConvertFrom(obj);
+        }
+    }
+
+    public class Converter : Converter<object> {
+        public override object Convert(object obj) {
+            return obj;
+        }
+        static readonly ConcurrentDictionary<Type, Lazy<IConverter>> _converters = new ConcurrentDictionary<Type, Lazy<IConverter>>();
+        static Converter() {
+            Register(() => new Converter());
+            Register(() => new NumberConverter<bool>());
+            Register(() => new NumberConverter<byte>());
+            Register(() => new NumberConverter<sbyte>());
+            Register(() => new NumberConverter<short>());
+            Register(() => new NumberConverter<ushort>());
+            Register(() => new NumberConverter<int>());
+            Register(() => new NumberConverter<uint>());
+            Register(() => new Int64Converter());
+            Register(() => new NumberConverter<ulong>());
+            Register(() => new NumberConverter<float>());
+            Register(() => new NumberConverter<double>());
+            Register(() => new NumberConverter<decimal>());
+            Register(() => new NumberConverter<IntPtr>());
+            Register(() => new NumberConverter<UIntPtr>());
+            Register(() => new NumberConverter<BigInteger>());
+        }
+
+        public static void Initialize() { }
+
+        public static void Register<T>(Func<Converter<T>> ctor) {
+            _converters[typeof(T)] = new Lazy<IConverter>(ctor);
+        }
+
+        private static Type GetConverterType(Type type) {
+            return typeof(Converter<>).MakeGenericType(type);
+        }
+
+        internal static IConverter GetInstance(Type type) {
+            return _converters.GetOrAdd(type, t => new Lazy<IConverter>(
+                () => Activator.CreateInstance(GetConverterType(t)) as IConverter
+            )).Value;
+        }
+    }
+}
