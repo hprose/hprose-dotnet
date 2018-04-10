af53ce53a763908dc5d55dc906f7cc4dee9401a7
andot <mabingyao@gmail.com>
2018-04-10 13:29:32 +0800

    Added Int16Deserializer

---
+++ src/Hprose.IO/Deserializers/Int16Deserializer.cs
@@ -0,0 +1,52 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * Int16Deserializer.cs                                   *
+ *                                                        *
+ * Int16Deserializer class for C#.                        *
+ *                                                        *
+ * LastModified: Apr 9, 2018                              *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+using Hprose.IO.Converters;
+
+using static Hprose.IO.HproseTags;
+
+namespace Hprose.IO.Deserializers {
+    class Int16Deserializer : Deserializer<short> {
+        public override short Read(Reader reader, int tag) {
+            if (tag >= '0' && tag <= '9') {
+                return (short)(tag - '0');
+            }
+            var stream = reader.Stream;
+            switch (tag) {
+                case TagInteger:
+                    return (short)ValueReader.ReadInt(stream);
+                case TagLong:
+                    return (short)ValueReader.ReadLong(stream);
+                case TagDouble:
+                    return (short)ValueReader.ReadDouble(stream);
+                case TagTrue:
+                    return 1;
+                case TagFalse:
+                case TagEmpty:
+                    return 0;
+                case TagUTF8Char:
+                    return Converter<short>.Instance.Convert(ValueReader.ReadUTF8Char(stream));
+                case TagString:
+                    return Converter<short>.Instance.Convert(ReferenceReader.ReadString(reader));
+                default:
+                    return base.Read(reader, tag);
+            }
+        }
+    }
+}
