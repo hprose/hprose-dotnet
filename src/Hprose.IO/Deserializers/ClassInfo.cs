4ce99e684a7b5313c77dbc2059c9d2dd15ebd7d9
andot <mabingyao@gmail.com>
2018-04-10 13:21:10 +0800

    Added ClassInfo

---
+++ src/Hprose.IO/Deserializers/ClassInfo.cs
@@ -0,0 +1,27 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * ClassInfo.cs                                           *
+ *                                                        *
+ * ClassInfo class for C#.                                *
+ *                                                        *
+ * LastModified: Apr 8, 2018                              *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+using System;
+
+namespace Hprose.IO.Deserializers {
+    struct ClassInfo {
+        public string Name;
+        public Type Type;
+        public string[] Members;
+    }
+}
