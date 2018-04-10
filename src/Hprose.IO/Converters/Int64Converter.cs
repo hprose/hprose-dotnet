871b0310bfa6dc378dab27742011b55978b984c6
andot <mabingyao@gmail.com>
2018-04-10 13:27:36 +0800

    Added Int64Converter

---
+++ src/Hprose.IO/Converters/Int64Converter.cs
@@ -0,0 +1,38 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * Int64Converter.cs                                      *
+ *                                                        *
+ * hprose Int64Converter class for C#.                    *
+ *                                                        *
+ * LastModified: Apr 10, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+using System;
+using System.Text;
+
+namespace Hprose.IO.Converters {
+    class Int64Converter : Converter<long> {
+        public override long Convert(object obj) {
+            switch (obj) {
+                case DateTime dt:
+                    return dt.Ticks;
+                case char[] chars:
+                    return base.Convert(new String(chars));
+                case StringBuilder sb:
+                    return base.Convert(sb.ToString());
+                default:
+                    return base.Convert(obj);
+            }
+        }
+    }
+}
