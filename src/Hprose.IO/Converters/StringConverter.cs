dbc0d497444061cb847f981a63e07cd626dc76db
andot <mabingyao@gmail.com>
2018-04-14 14:04:29 +0800

    Added StringConverter

---
+++ src/Hprose.IO/Converters/StringConverter.cs
@@ -0,0 +1,43 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * StringConverter.cs                                     *
+ *                                                        *
+ * hprose StringConverter class for C#.                   *
+ *                                                        *
+ * LastModified: Apr 14, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+using System;
+using System.Text;
+
+namespace Hprose.IO.Converters {
+    class StringConverter : Converter<string> {
+        public override string Convert(object obj) {
+            switch (obj) {
+                case String s:
+                    return s;
+                case char[] chars:
+                    return new String(chars);
+                case byte[] bytes:
+                    try {
+                        return Encoding.UTF8.GetString(bytes);
+                    }
+                    catch(Exception) {
+                        return Encoding.Default.GetString(bytes);
+                    }
+                default:
+                    return obj.ToString();
+            }
+        }
+    }
+}
