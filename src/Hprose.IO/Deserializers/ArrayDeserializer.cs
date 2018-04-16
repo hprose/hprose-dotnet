9761536bc1ba9bc3b498a6c3fdef71454070a259
andot <mabingyao@gmail.com>
2018-04-16 20:26:38 +0800

    Changed EmptyArray to static field

--- src/Hprose.IO/Deserializers/ArrayDeserializer.cs
+++ src/Hprose.IO/Deserializers/ArrayDeserializer.cs
@@ -8,9 +8,9 @@
 \**********************************************************/
 /**********************************************************\
  *                                                        *
- * BytesDeserializer.cs                                   *
+ * ArrayDeserializer.cs                                   *
  *                                                        *
- * BytesDeserializer class for C#.                        *
+ * ArrayDeserializer class for C#.                        *
  *                                                        *
  * LastModified: Apr 15, 2018                             *
  * Author: Ma Bingyao <andot@hprose.com>                  *
@@ -21,7 +21,7 @@ using static Hprose.IO.HproseTags;
 
 namespace Hprose.IO.Deserializers {
     class ArrayDeserializer<T> : Deserializer<T[]> {
-        private readonly T[] EmptyArray = new T[0] { };
+        private static readonly T[] EmptyArray = new T[0] { };
         public override T[] Read(Reader reader, int tag) {
             var stream = reader.Stream;
             switch (tag) {
@@ -35,12 +35,12 @@ namespace Hprose.IO.Deserializers {
         }
     }
     class Array2Deserializer<T> : Deserializer<T[,]> {
-        private readonly T[,] EmptyArray2 = new T[0, 0] { };
+        private static readonly T[,] EmptyArray = new T[0, 0] { };
         public override T[,] Read(Reader reader, int tag) {
             var stream = reader.Stream;
             switch (tag) {
                 case TagEmpty:
-                    return EmptyArray2;
+                    return EmptyArray;
                 case TagList:
                     return ReferenceReader.ReadArray2<T>(reader);
                 default:
