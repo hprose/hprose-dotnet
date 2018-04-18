1d43514e3478dd6e6eeedd52db3b98e9cc2ab3cf
andot <mabingyao@gmail.com>
2018-04-18 21:29:05 +0800

    Added CharType.cs

---
+++ src/Hprose.IO/Deserializers/CharType.cs
@@ -0,0 +1,24 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * CharType.cs                                            *
+ *                                                        *
+ * hprose CharType enum for C#.                           *
+ *                                                        *
+ * LastModified: Apr 18, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose.IO.Deserializers {
+    public enum CharType {
+        String, Char
+    }
+}
