30945e181e69c5a66fed06ec2222f018b690576e
andot <mabingyao@gmail.com>
2018-04-18 21:29:38 +0800

    Added LongType.cs

---
+++ src/Hprose.IO/Deserializers/LongType.cs
@@ -0,0 +1,24 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * LongType.cs                                            *
+ *                                                        *
+ * hprose LongType enum for C#.                           *
+ *                                                        *
+ * LastModified: Apr 18, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose.IO.Deserializers {
+    public enum LongType {
+        BigInteger, Int64, UInt64
+    }
+}
