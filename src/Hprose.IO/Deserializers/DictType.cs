c3d325d5d345f590611a8eff67b60e1e005ac228
andot <mabingyao@gmail.com>
2018-04-18 21:29:18 +0800

    Added DictType.cs

---
+++ src/Hprose.IO/Deserializers/DictType.cs
@@ -0,0 +1,24 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * DictType.cs                                            *
+ *                                                        *
+ * hprose DictType enum for C#.                           *
+ *                                                        *
+ * LastModified: Apr 18, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose.IO.Deserializers {
+    public enum DictType {
+        NullableKeyDictionary, Dictionary, ExpandoObject, Hashtable
+    }
+}
