1e5d6cca5b6812f40dfbf8fcefeed66016a2ee2c
andot <mabingyao@gmail.com>
2018-04-18 21:29:50 +0800

    Added RealType.cs

---
+++ src/Hprose.IO/Deserializers/RealType.cs
@@ -0,0 +1,24 @@
+﻿/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * RealType.cs                                            *
+ *                                                        *
+ * hprose RealType enum for C#.                           *
+ *                                                        *
+ * LastModified: Apr 18, 2018                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose.IO.Deserializers {
+    public enum RealType {
+        Double, Single, Decimal
+    }
+}
