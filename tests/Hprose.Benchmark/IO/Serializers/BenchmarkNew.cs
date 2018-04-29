c238f1c1ac7388294ce5770292279e42b9eef1f5
andot <mabingyao@gmail.com>
2018-04-29 15:22:15 +0800

    Improved BenchmarkNew

--- tests/Hprose.Benchmark/IO/Serializers/BenchmarkNew.cs
+++ tests/Hprose.Benchmark/IO/Serializers/BenchmarkNew.cs
@@ -1,6 +1,6 @@
 ﻿
 using System;
-
+using System.Linq.Expressions;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Attributes.Columns;
 using BenchmarkDotNet.Attributes.Exporters;
@@ -14,6 +14,7 @@ namespace Hprose.Benchmark.IO.Serializers {
         public T New<T>() where T : new() => new T();
         public T CreateInstance<T>() => (T)Activator.CreateInstance(typeof(T));
         public T CreateInstance2<T>() => (T)Activator.CreateInstance(typeof(T), true);
+        public T ExpressionNew<T>() => Expression.Lambda<Func<T>>(Expression.New(typeof(T))).Compile()();
         [Benchmark]
         public object BenchNew() => New<object>();
         [Benchmark]
@@ -22,5 +23,7 @@ namespace Hprose.Benchmark.IO.Serializers {
         public object BenchCreateInstance2() => CreateInstance2<object>();
         [Benchmark]
         public object BenchFactoryNew() => Factory<object>.New();
+        [Benchmark]
+        public object BenchExpressionNew() => ExpressionNew<object>();
     }
 }
