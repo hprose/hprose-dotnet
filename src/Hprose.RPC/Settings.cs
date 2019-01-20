a3b96ed0fc30ed0add9c05cf43d33a0f12305ae4
小马哥 <mabingyao@gmail.com>
2019-01-21 01:11:26 +0800

    Create Settings.cs

---
+++ src/Hprose.RPC/Settings.cs
@@ -0,0 +1,34 @@
+﻿/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+|  Settings.cs                                             |
+|                                                          |
+|  Settings class for C#.                                  |
+|                                                          |
+|  LastModified: Jan 19, 2019                              |
+|  Author: Ma Bingyao <andot@hprose.com>                   |
+|                                                          |
+\*________________________________________________________*/
+
+using Hprose.IO;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Hprose.RPC {
+    public class Settings {
+        public bool? Simple { get; set; } = null;
+        public Mode? Mode { get; set; } = null;
+        public LongType? LongType { get; set; } = null;
+        public RealType? RealType { get; set; } = null;
+        public CharType? CharType { get; set; } = null;
+        public ListType? ListType { get; set; } = null;
+        public DictType? DictType { get; set; } = null;
+        public Type Type { get; set; } = null;
+        public ExpandoObject RequestHeaders { get; set; } = null;
+        public IDictionary<string, object> Context { get; set; } = null;
+    }
+}
