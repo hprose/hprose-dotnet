a528b6f6c4ee097e3dc80ceccb73400224c8d985
小马哥 <mabingyao@gmail.com>
2019-01-21 01:10:56 +0800

    Create Context.cs

---
+++ src/Hprose.RPC/Context.cs
@@ -0,0 +1,51 @@
+﻿/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+|  Context.cs                                              |
+|                                                          |
+|  Context class for C#.                                   |
+|                                                          |
+|  LastModified: Jan 19, 2019                              |
+|  Author: Ma Bingyao <andot@hprose.com>                   |
+|                                                          |
+\*________________________________________________________*/
+
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Hprose.RPC {
+    public class Context : DynamicObject, ICloneable {
+        public ExpandoObject RequestHeaders { get; } = new ExpandoObject();
+        public ExpandoObject ResponseHeaders { get; } = new ExpandoObject();
+        protected readonly Dictionary<string, object> items = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result) {
+            return items.TryGetValue(binder.Name, out result);
+
+        }
+        public override bool TrySetMember(SetMemberBinder binder, object value) {
+            items[binder.Name] = value;
+            return true;
+        }
+        public override bool TryDeleteMember(DeleteMemberBinder binder) {
+            try {
+                items.Remove(binder.Name);
+                return true;
+            }
+            catch {
+                return false;
+            }
+        }
+        public bool Contains(string name) {
+            return items.ContainsKey(name);
+        }
+
+        public object Clone() {
+            return MemberwiseClone();
+        }
+    }
+}
