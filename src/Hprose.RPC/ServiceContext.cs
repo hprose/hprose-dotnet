7f4b7c13795389c3e82f2c1a6466679d7aa81cfa
小马哥 <mabingyao@gmail.com>
2019-01-29 01:55:55 +0800

    Create ServiceContext.cs

---
+++ src/Hprose.RPC/ServiceContext.cs
@@ -0,0 +1,28 @@
+﻿/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+|  ServiceContext.cs                                       |
+|                                                          |
+|  ServiceContext class for C#.                            |
+|                                                          |
+|  LastModified: Jan 27, 2019                              |
+|  Author: Ma Bingyao <andot@hprose.com>                   |
+|                                                          |
+\*________________________________________________________*/
+
+using Hprose.IO;
+using System;
+using System.Collections.Generic;
+
+namespace Hprose.RPC {
+    public class ServiceContext : Context {
+        public Service Service { get; private set; }
+        public Method Method { get; set; } = null;
+        public ServiceContext(Service service) {
+            Service = service;
+        }
+    }
+}
