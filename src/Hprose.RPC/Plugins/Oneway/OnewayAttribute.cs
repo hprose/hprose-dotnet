94f0bfcc55e72ee4fc34ec2f6297bb2289c2bd32
小马哥 <mabingyao@gmail.com>
2019-02-02 02:04:47 +0800

    Added Oneway Plugin

---
+++ src/Hprose.RPC/Plugins/Oneway/OnewayAttribute.cs
@@ -0,0 +1,23 @@
+﻿/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+|  OnewayAttribute.cs                                      |
+|                                                          |
+|  Oneway Attribute for C#.                                |
+|                                                          |
+|  LastModified: Feb 2, 2019                               |
+|  Author: Ma Bingyao <andot@hprose.com>                   |
+|                                                          |
+\*________________________________________________________*/
+
+using System;
+
+namespace Hprose.RPC.Plugins.Oneway {
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class OnewayAttribute : ContextAttribute {
+        public OnewayAttribute() : base("Oneway", true) { }
+    }
+}
