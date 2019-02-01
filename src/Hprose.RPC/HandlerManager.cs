faeb1c0914e7ca7f755a38d2afcab486b7e35044
小马哥 <mabingyao@gmail.com>
2019-02-01 10:40:10 +0800

    Update HandlerManager.cs
    Threads safe.

--- src/Hprose.RPC/HandlerManager.cs
+++ src/Hprose.RPC/HandlerManager.cs
@@ -8,13 +8,14 @@
 |                                                          |
 |  HandlerManager class for C#.                            |
 |                                                          |
-|  LastModified: Jan 20, 2019                              |
+|  LastModified: Feb 1, 2019                               |
 |  Author: Ma Bingyao <andot@hprose.com>                   |
 |                                                          |
 \*________________________________________________________*/
 
 using System.Collections.Generic;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Hprose.RPC {
@@ -25,6 +26,8 @@ namespace Hprose.RPC {
     public class HandlerManager {
         private List<InvokeHandler> invokeHandlers = new List<InvokeHandler>();
         private List<IOHandler> ioHandlers = new List<IOHandler>();
+        private ReaderWriterLockSlim invokeHandlersReaderWriterLock = new ReaderWriterLockSlim();
+        private ReaderWriterLockSlim ioHandlersReaderWriterLock = new ReaderWriterLockSlim();
         private readonly NextInvokeHandler defaultInvokeHandler;
         private readonly NextIOHandler defaultIOHandler;
         public NextInvokeHandler InvokeHandler { get; private set; }
@@ -41,40 +44,52 @@ namespace Hprose.RPC {
         }
         private void RebuildInvokeHandler() {
             var next = defaultInvokeHandler;
+            invokeHandlersReaderWriterLock.EnterReadLock();
             var n = invokeHandlers.Count;
             for (var i = n - 1; i >= 0; --i) {
                 next = GetNextInvokeHandler(invokeHandlers[i], next);
             }
+            invokeHandlersReaderWriterLock.ExitReadLock();
             InvokeHandler = next;
         }
         private void RebuildIOHandler() {
             var next = defaultIOHandler;
+            ioHandlersReaderWriterLock.EnterReadLock();
             var n = ioHandlers.Count;
             for (var i = n - 1; i >= 0; --i) {
                 next = GetNextIOHandler(ioHandlers[i], next);
             }
+            ioHandlersReaderWriterLock.ExitReadLock();
             IOHandler = next;
         }
         public void Use(params InvokeHandler[] handlers) {
+            invokeHandlersReaderWriterLock.EnterWriteLock();
             invokeHandlers.AddRange(handlers);
+            invokeHandlersReaderWriterLock.ExitWriteLock();
             RebuildInvokeHandler();
         }
         public void Use(params IOHandler[] handlers) {
+            ioHandlersReaderWriterLock.EnterWriteLock();
             ioHandlers.AddRange(handlers);
+            ioHandlersReaderWriterLock.ExitWriteLock();
             RebuildIOHandler();
         }
         public void Unuse(params InvokeHandler[] handlers) {
             bool rebuild = false;
+            invokeHandlersReaderWriterLock.EnterWriteLock();
             for (int i = 0, n = handlers.Length; i < n; ++i) {
                 rebuild = rebuild || invokeHandlers.Remove(handlers[i]);
             }
+            invokeHandlersReaderWriterLock.ExitWriteLock();
             if (rebuild) RebuildInvokeHandler();
         }
         public void Unuse(params IOHandler[] handlers) {
             bool rebuild = false;
+            ioHandlersReaderWriterLock.EnterWriteLock();
             for (int i = 0, n = handlers.Length; i < n; ++i) {
                 rebuild = rebuild || ioHandlers.Remove(handlers[i]);
             }
+            ioHandlersReaderWriterLock.ExitWriteLock();
             if (rebuild) RebuildIOHandler();
         }
     }
