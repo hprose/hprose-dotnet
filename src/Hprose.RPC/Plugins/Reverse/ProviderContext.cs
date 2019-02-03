0a427ace585fc0cdca09f295cf7d3db0d7d84fe8
小马哥 <mabingyao@gmail.com>
2019-02-04 01:26:09 +0800

    Create ProviderContext.cs

---
+++ src/Hprose.RPC/Plugins/Reverse/ProviderContext.cs
@@ -0,0 +1,25 @@
+﻿/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+|  ProviderContext.cs                                      |
+|                                                          |
+|  ProviderContext class for C#.                           |
+|                                                          |
+|  LastModified: Feb 3, 2019                               |
+|  Author: Ma Bingyao <andot@hprose.com>                   |
+|                                                          |
+\*________________________________________________________*/
+
+namespace Hprose.RPC.Plugins.Reverse {
+    public class ProviderContext : Context {
+        public Client Client { get; private set; }
+        public Method Method { get; private set; }
+        public ProviderContext(Client client, Method method) {
+            Client = client;
+            Method = method;
+        }
+    }
+}
