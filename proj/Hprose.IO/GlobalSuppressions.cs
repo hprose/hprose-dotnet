09eba8c15fc975210522715d273120b82804da69
小马哥 <mabingyao@gmail.com>
2019-02-07 04:13:41 +0800

    Fixed some warnings

---
+++ proj/Hprose.IO/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1810:Initialize reference type static fields inline", Justification = "<挂起>", Scope = "member", Target = "~M:Hprose.IO.Converter`1.#cctor")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1000:Do not declare static members on generic types", Justification = "<挂起>", Scope = "member", Target = "~M:Hprose.IO.Converter`1.Convert``1(``0)~`0")]
---
+++ proj/Hprose.RPC/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1716:Identifiers should not match keywords", Justification = "<挂起>", Scope = "member", Target = "~M:Hprose.RPC.HandlerManager`2.GetNextHandler(`0,`1)~`1")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "<挂起>", Scope = "member", Target = "~M:Hprose.RPC.HttpHandler.ClientAccessPolicyXmlHandler(System.Net.HttpListenerRequest,System.Net.HttpListenerResponse)~System.Threading.Tasks.Task{System.Boolean}")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "<挂起>", Scope = "member", Target = "~M:Hprose.RPC.HttpHandler.CrossDomainXmlHandler(System.Net.HttpListenerRequest,System.Net.HttpListenerResponse)~System.Threading.Tasks.Task{System.Boolean}")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "<挂起>", Scope = "member", Target = "~M:Hprose.RPC.HttpHandler.GetOutputStream(System.Net.HttpListenerRequest,System.Net.HttpListenerResponse)~System.IO.Stream")]
