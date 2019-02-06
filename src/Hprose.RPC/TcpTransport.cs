ec130a8ee8c3d43de5c132d0bab08e9e2b8368f5
小马哥 <mabingyao@gmail.com>
2019-02-06 16:10:15 +0800

    Update TcpTransport.cs

--- src/Hprose.RPC/TcpTransport.cs
+++ src/Hprose.RPC/TcpTransport.cs
@@ -32,38 +32,65 @@ namespace Hprose.RPC {
         public TimeSpan Timeout { get; set; } = new TimeSpan(0, 0, 30);
         public int ReceiveBufferSize { get; set; } = 8192;
         public int SendBufferSize { get; set; } = 8192;
-        public int MaxResponseContentBufferSize { get; set; } = int.MaxValue;
         private ConcurrentDictionary<string, ConcurrentQueue<(int, Stream)>> Requests { get; } = new ConcurrentDictionary<string, ConcurrentQueue<(int, Stream)>>();
         private ConcurrentDictionary<string, ConcurrentDictionary<int, TaskCompletionSource<Stream>>> Results { get; } = new ConcurrentDictionary<string, ConcurrentDictionary<int, TaskCompletionSource<Stream>>>();
-        private ConcurrentDictionary<string, ConcurrentDictionary<int, TaskCompletionSource<Stream>>> ReadyResults { get; } = new ConcurrentDictionary<string, ConcurrentDictionary<int, TaskCompletionSource<Stream>>>();
+        private ConcurrentDictionary<string, ConcurrentDictionary<int, TaskCompletionSource<Stream>>> Sended { get; } = new ConcurrentDictionary<string, ConcurrentDictionary<int, TaskCompletionSource<Stream>>>();
         private ConcurrentDictionary<string, Lazy<Task<TcpClient>>> TcpClients { get; } = new ConcurrentDictionary<string, Lazy<Task<TcpClient>>>();
         private readonly Func<string, Lazy<Task<TcpClient>>> tcpClientFactory;
         public TcpTransport() {
             tcpClientFactory = (uri) => {
                 return new Lazy<Task<TcpClient>>(async () => {
-                    var u = new Uri(uri);
-                    var family = u.Scheme.Last() == '6' ? AddressFamily.InterNetworkV6 : AddressFamily.InterNetwork;
+                    try {
+                        var u = new Uri(uri);
+                        var family = u.Scheme.Last() == '6' ? AddressFamily.InterNetworkV6 : AddressFamily.InterNetwork;
 #if NET40 || NET45 || NET451 || NET452
-                    var host = u.DnsSafeHost;
+                        var host = u.DnsSafeHost;
 #else
-                    var host = u.IdnHost;
+                        var host = u.IdnHost;
 #endif
-                    var port = u.Port > 0 ? u.Port : 8412;
-                    var client = new TcpClient(family) {
-                        LingerState = LingerState,
-                        NoDelay = NoDelay,
-                        ReceiveBufferSize = ReceiveBufferSize,
-                        SendBufferSize = SendBufferSize
-                    };
-                    await client.ConnectAsync(host, port);
-                    Receive(uri, client);
-                    return client;
+                        var port = u.Port > 0 ? u.Port : 8412;
+                        var client = new TcpClient(family) {
+                            LingerState = LingerState,
+                            NoDelay = NoDelay,
+                            ReceiveBufferSize = ReceiveBufferSize,
+                            SendBufferSize = SendBufferSize
+                        };
+                        await client.ConnectAsync(host, port);
+                        Receive(uri, client);
+                        return client;
+                    }
+                    catch (Exception e) {
+                        var requests = Requests[uri];
+                        var sended = Sended[uri];
+                        var results = Results[uri];
+                        if (requests != null) {
+                            while (!requests.IsEmpty) {
+                                if (requests.TryDequeue(out (int index, Stream stream) request)) {
+                                    if (sended.TryRemove(request.index, out var result)) {
+                                        result.TrySetException(e);
+                                    }
+                                    if (results.TryRemove(request.index, out result)) {
+                                        result.TrySetException(e);
+                                    }
+                                }
+                            }
+                        }
+                        throw e;
+                    }
                 });
             };
         }
-        public void Abort() {
-            foreach (var tcpClient in TcpClients.Values) {
-
+        public async void Abort() {
+            foreach (var LazyTcpClient in TcpClients.Values) {
+                try {
+                    var tcpClient = await LazyTcpClient.Value;
+#if NET40 || NET45 || NET451 || NET452
+                    tcpClient.Close();
+#else
+                    tcpClient.Dispose();
+#endif
+                }
+                catch { }
             }
         }
         public async Task<byte[]> ReadAsync(Stream stream, byte[] bytes, int offset, int length) {
@@ -75,11 +102,11 @@ namespace Hprose.RPC {
             return bytes;
         }
         public async void Receive(string uri, TcpClient tcpClient) {
-            var netStream = tcpClient.GetStream();
-            var results = ReadyResults[uri];
+            var sended = Sended[uri];
             var header = new byte[12];
-            while (true) {
-                try {
+            try {
+                var netStream = tcpClient.GetStream();
+                while (true) {
                     await ReadAsync(netStream, header, 0, 12);
                     uint crc = (uint)((header[0] << 24) | (header[1] << 16) | (header[2] << 8) | header[3]);
                     if (CRC32.Compute(header, 4, 8) != crc || (header[4] & 0x80) == 0) {
@@ -89,54 +116,56 @@ namespace Hprose.RPC {
                     int index = (header[4] << 24) | (header[5] << 16) | (header[6] << 8) | header[7];
                     bool has_error = (index & 0x80000000) != 0;
                     index &= 0x7FFFFFFF;
-                    results.TryRemove(index, out var result);
-                    if (has_error) {
-                        if (result != null) {
-                            result.TrySetException(new Exception(Encoding.UTF8.GetString(await ReadAsync(netStream, new byte[length], 0, length))));
+                    var data = await ReadAsync(netStream, new byte[length], 0, length);
+                    if (sended.TryRemove(index, out var result)) {
+                        if (has_error) {
+                            result.TrySetException(new Exception(Encoding.UTF8.GetString(data)));
+                            throw new IOException("connection closed");
                         }
-                        throw new IOException("connection closed");
+                        result.TrySetResult(new MemoryStream(data));
                     }
-                    if (length > MaxResponseContentBufferSize) {
-                        if (result != null) {
-                            result.TrySetException(new IOException("response too long"));
-                        }
-                        throw new IOException("connection closed");
-                    }
-                    if (result != null) {
-                        result.TrySetResult(new MemoryStream(await ReadAsync(netStream, new byte[length], 0, length)));
-                    }
-                }
-                catch (Exception e) {
-                    foreach (var index in results.Keys) {
-                        results.TryRemove(index, out var result);
-                        result.TrySetException(e);
-                    }
-#if NET40 || NET45 || NET451 || NET452
-                    tcpClient.Close();
-#else
-                    tcpClient.Dispose();
-#endif
-                    return;
                 }
             }
+            catch (Exception e) {
+                foreach (var index in sended.Keys) {
+                    if (sended.TryRemove(index, out var result)) {
+                        result.TrySetException(e);
+                    }
+                }
+#if NET40 || NET45 || NET451 || NET452
+                tcpClient.Close();
+#else
+                tcpClient.Dispose();
+#endif
+            }
         }
-        public async void Send(string uri, TcpClient tcpClient) {
-            var netStream = tcpClient.GetStream();
+        public async void Send(string uri) {
             var requests = Requests[uri];
             var results = Results[uri];
-            var readyResults = ReadyResults[uri];
+            var sended = Sended.GetOrAdd(uri, (_) => new ConcurrentDictionary<int, TaskCompletionSource<Stream>>());
             var header = new byte[12];
-            do {
+            while (!requests.IsEmpty) {
                 requests.TryPeek(out (int index, Stream stream) request);
                 var index = request.index;
                 if (Interlocked.Exchange(ref current, index) == index) return;
-                results.TryRemove(index, out var temp);
-                readyResults[index] = temp;
+                if (results.TryGetValue(index, out var result)) {
+                    sended[index] = result;
+                    results.TryRemove(index, out result);
+                }
                 var stream = request.stream;
-                if (!stream.CanSeek) {
-                    stream = new MemoryStream();
-                    await request.stream.CopyToAsync(stream);
-                    request.stream.Dispose();
+                try {
+                    if (!stream.CanSeek) {
+                        stream = new MemoryStream();
+                        await request.stream.CopyToAsync(stream);
+                        request.stream.Dispose();
+                    }
+                }
+                catch (Exception e) {
+                    if (sended.TryRemove(index, out var value)) {
+                        value.TrySetException(e);
+                    }
+                    requests.TryDequeue(out request);
+                    continue;
                 }
                 var n = (int)stream.Length;
                 header[4] = (byte)(n >> 24 & 0xFF | 0x80);
@@ -152,25 +181,29 @@ namespace Hprose.RPC {
                 header[1] = (byte)(crc32 >> 16 & 0xFF);
                 header[2] = (byte)(crc32 >> 8 & 0xFF);
                 header[3] = (byte)(crc32 & 0xFF);
+                TcpClient tcpClient = null;
                 try {
+                    tcpClient = await GetTcpClient(uri);
+                    var netStream = tcpClient.GetStream();
                     await netStream.WriteAsync(header, 0, 12);
                     await stream.CopyToAsync(netStream);
                 }
                 catch (Exception e) {
-                    foreach (int i in readyResults.Keys) {
-                        results.TryRemove(i, out var result);
-                        result.TrySetException(e);
+                    foreach (var i in sended.Keys) {
+                        if (sended.TryRemove(i, out var value)) {
+                            value.TrySetException(e);
+                        }
                     }
+                    if (tcpClient != null) {
 #if NET40 || NET45 || NET451 || NET452
-                    tcpClient.Close();
+                        tcpClient.Close();
 #else
-                    tcpClient.Dispose();
+                        tcpClient.Dispose();
 #endif
-                    requests.TryDequeue(out request);
-                    return;
+                    }
                 }
                 requests.TryDequeue(out request);
-            } while (!requests.IsEmpty);
+            }
         }
         public async Task<Stream> Transport(Stream request, Context context) {
             var clientContext = context as ClientContext;
@@ -187,13 +220,12 @@ namespace Hprose.RPC {
             requests.Enqueue((index, request));
             requests.TryPeek(out (int index, Stream stream) first);
             if (first.index == index) {
-                ReadyResults.GetOrAdd(uri, (_) => new ConcurrentDictionary<int, TaskCompletionSource<Stream>>());
-                TcpClient tcpClient = await GetTcpClient(uri);
-                Send(uri, tcpClient);
+                Send(uri);
             }
             return await result.Task;
         }
         private async Task<TcpClient> GetTcpClient(string uri) {
+            int retried = 0;
             while(true) {
                 var LazyTcpClient = TcpClients.GetOrAdd(uri, tcpClientFactory);
                 var tcpClient = await LazyTcpClient.Value;
@@ -201,13 +233,16 @@ namespace Hprose.RPC {
                     var available = tcpClient.Available;
                     return tcpClient;
                 }
-                catch {
-                    TcpClients.TryUpdate(uri, tcpClientFactory(uri), LazyTcpClient);
+                catch (Exception e) {
+                    TcpClients.TryRemove(uri, out LazyTcpClient);
 #if NET40 || NET45 || NET451 || NET452
                     tcpClient.Close();
 #else
                     tcpClient.Dispose();
 #endif
+                    if (++retried > 1) {
+                        throw e;
+                    }
                 }
             }
         }
