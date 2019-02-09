d233c9fdc3d579279a9343f6d9fff490862bb12f
小马哥 <mabingyao@gmail.com>
2019-02-10 00:11:43 +0800

    Update Method.cs

--- src/Hprose.RPC/Method.cs
+++ src/Hprose.RPC/Method.cs
@@ -13,9 +13,6 @@
 |                                                          |
 \*________________________________________________________*/
 
-using Hprose.IO;
-using System;
-using System.Collections.Generic;
 using System.Reflection;
 
 namespace Hprose.RPC {
