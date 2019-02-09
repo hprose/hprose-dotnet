27f1f016427a75c664e22b820d09a85ef1f859fc
小马哥 <mabingyao@gmail.com>
2019-02-09 19:13:12 +0800

    Moved MemoryStreamExtensions to StreamExtensions

--- src/Hprose.IO/StreamExtensions.cs
+++ src/Hprose.IO/StreamExtensions.cs
@@ -4,20 +4,21 @@
 |                                                          |
 | Official WebSite: https://hprose.com                     |
 |                                                          |
-|  MemoryStreamExtensions.cs                               |
+|  StreamExtensions.cs                                     |
 |                                                          |
-|  MemoryStream Extensions for C#.                         |
+|  Stream Extensions for C#.                               |
 |                                                          |
-|  LastModified: Feb 7, 2019                               |
+|  LastModified: Feb 9, 2019                               |
 |  Author: Ma Bingyao <andot@hprose.com>                   |
 |                                                          |
 \*________________________________________________________*/
 
 using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace Hprose.IO {
-    public static class MemoryStreamExtensions {
+    public static class StreamExtensions {
 #if NET40 || NET45 || NET451 || NET452
         public static ArraySegment<byte> GetArraySegment(this MemoryStream stream) {
             var bytes = stream.ToArray();
@@ -32,5 +33,13 @@ namespace Hprose.IO {
             return new ArraySegment<byte>(bytes, 0, bytes.Length);
         }
 #endif
+        public static async Task<MemoryStream> ToMemoryStream(this Stream stream) {
+            if (stream is MemoryStream) return stream as MemoryStream;
+            MemoryStream memoryStream = new MemoryStream();
+            await stream.CopyToAsync(memoryStream).ConfigureAwait(false);
+            memoryStream.Position = 0;
+            stream.Dispose();
+            return memoryStream;
+        }
     }
 }
