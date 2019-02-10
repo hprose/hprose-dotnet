cfbfdc41fc23bf833a14f48f0d07492ca2b80e7d
小马哥 <mabingyao@gmail.com>
2019-02-11 03:30:43 +0800

    Update WebSocketHandler.cs

--- src/Hprose.RPC/WebSocketHandler.cs
+++ src/Hprose.RPC/WebSocketHandler.cs
@@ -37,58 +37,66 @@ namespace Hprose.RPC {
                 }
                 int index = response.index;
                 MemoryStream stream = response.stream;
-                header[0] = (byte)(index >> 24 & 0xFF);
-                header[1] = (byte)(index >> 16 & 0xFF);
-                header[2] = (byte)(index >> 8 & 0xFF);
-                header[3] = (byte)(index & 0xFF);
-                await webSocket.SendAsync(new ArraySegment<byte>(header), WebSocketMessageType.Binary, false, CancellationToken.None).ConfigureAwait(false);
-                await webSocket.SendAsync(stream.GetArraySegment(), WebSocketMessageType.Binary, true, CancellationToken.None).ConfigureAwait(false);
-                if ((index & 0x80000000) != 0) {
-                    var data = (stream as MemoryStream).GetArraySegment();
-                    var message = Encoding.UTF8.GetString(data.Array, data.Offset, data.Count);
-                    stream.Dispose();
-                    throw new Exception(message);
-                }
-                stream.Dispose();
-            }
-        }
-        private async void Run(ConcurrentQueue<(int index, MemoryStream stream)> responses, int index, ArraySegment<byte> data, Context context) {
-            using (var request = new MemoryStream(data.Array, data.Offset, data.Count, false, true)) {
-                MemoryStream response = null;
                 try {
-                    response = await (await Service.Handle(request, context).ConfigureAwait(false)).ToMemoryStream().ConfigureAwait(false);
-                }
-                catch (Exception e) {
-                    index = (int)(index | 0x80000000);
-                    var bytes = Encoding.UTF8.GetBytes(e.Message);
-                    response = new MemoryStream(bytes, 0, bytes.Length, false, true);
+                    header[0] = (byte)(index >> 24 & 0xFF);
+                    header[1] = (byte)(index >> 16 & 0xFF);
+                    header[2] = (byte)(index >> 8 & 0xFF);
+                    header[3] = (byte)(index & 0xFF);
+                    await webSocket.SendAsync(new ArraySegment<byte>(header), WebSocketMessageType.Binary, false, CancellationToken.None).ConfigureAwait(false);
+                    await webSocket.SendAsync(stream.GetArraySegment(), WebSocketMessageType.Binary, true, CancellationToken.None).ConfigureAwait(false);
+                    if ((index & 0x80000000) != 0) {
+                        var data = (stream as MemoryStream).GetArraySegment();
+                        var message = Encoding.UTF8.GetString(data.Array, data.Offset, data.Count);
+                        throw new Exception(message);
+                    }
                 }
                 finally {
-                    responses.Enqueue((index, response));
+                    stream.Dispose();
                 }
             }
         }
-        private async Task<MemoryStream> ReadAsync(WebSocket webSocket, ConcurrentQueue<(int index, MemoryStream stream)> responses) {
+        private async void Run(ConcurrentQueue<(int index, MemoryStream stream)> responses, int index, MemoryStream request, Context context) {
+            MemoryStream response = null;
+            try {
+                response = await (await Service.Handle(request, context).ConfigureAwait(false)).ToMemoryStream().ConfigureAwait(false);
+            }
+            catch (Exception e) {
+                index = (int)(index | 0x80000000);
+                var bytes = Encoding.UTF8.GetBytes(e.Message);
+                response = new MemoryStream(bytes, 0, bytes.Length, false, true);
+            }
+            finally {
+                responses.Enqueue((index, response));
+                request.Dispose();
+            }
+        }
+        private async Task<(int, MemoryStream)> ReadAsync(WebSocket webSocket, ConcurrentQueue<(int index, MemoryStream stream)> responses) {
             MemoryStream stream = new MemoryStream();
             var buffer = ArrayPool<byte>.Shared.Rent(16384);
+            var index = -1;
             try {
                 while (true) {
                     var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None).ConfigureAwait(false);
                     if (result.CloseStatus != null) {
                         throw new WebSocketException((int)result.CloseStatus, result.CloseStatusDescription);
                     }
-                    stream.Write(buffer, 0, result.Count);
+                    if (index < 0) {
+                        index = (buffer[0] << 24) | (buffer[1] << 16) | (buffer[2] << 8) | buffer[3];
+                        stream.Write(buffer, 4, result.Count - 4);
+                    }
+                    else {
+                        stream.Write(buffer, 0, result.Count);
+                    }
                     if (stream.Length > Service.MaxRequestLength) {
                         var data = stream.GetArraySegment();
-                        int index = (data.Array[data.Offset] << 24) |
-                                    (data.Array[data.Offset + 1] << 16) |
-                                    (data.Array[data.Offset + 2] << 8) |
-                                    data.Array[data.Offset + 3];
                         var bytes = Encoding.UTF8.GetBytes("request too long");
                         responses.Enqueue(((int)(index | 0x80000000), new MemoryStream(bytes, 0, bytes.Length, false, true)));
-                        return null;
+                        return (index, null);
                     }
-                    if (result.EndOfMessage) return stream;
+                    if (result.EndOfMessage) {
+                        stream.Position = 0;
+                        return (index, stream);
+                    }
                 }
             }
             finally {
@@ -97,14 +105,9 @@ namespace Hprose.RPC {
         }
         public async Task Receive(WebSocket webSocket, ServiceContext context, ConcurrentQueue<(int index, MemoryStream stream)> responses) {
             while (true) {
-                MemoryStream stream = await ReadAsync(webSocket, responses).ConfigureAwait(false);
+                var (index, stream) = await ReadAsync(webSocket, responses).ConfigureAwait(false);
                 if (stream == null) return;
-                var buffer = stream.GetArraySegment();
-                int index = (buffer.Array[buffer.Offset] << 24) |
-                            (buffer.Array[buffer.Offset + 1] << 16) |
-                            (buffer.Array[buffer.Offset + 2] << 8) |
-                            buffer.Array[buffer.Offset + 3];
-                Run(responses, index, new ArraySegment<byte>(buffer.Array, buffer.Offset + 4, buffer.Count - 4), context.Clone() as Context);
+                Run(responses, index, stream, context.Clone() as Context);
             }
         }
         public override async void Handler(HttpListenerContext httpContext) {
