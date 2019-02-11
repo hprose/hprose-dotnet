6d95edc75541b4014f56cf2421d703d9e653ca0c
小马哥 <mabingyao@gmail.com>
2019-02-11 22:37:10 +0800

    Added Hprose.RPC.AspNet

---
+++ src/Hprose.RPC.AspNet/AspNetHttpHandler.cs
@@ -0,0 +1,27 @@
+﻿/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+|  AspNetHttpHandler.cs                                    |
+|                                                          |
+|  AspNetHttpHandler class for C#.                         |
+|                                                          |
+|  LastModified: Feb 11, 2019                              |
+|  Author: Ma Bingyao <andot@hprose.com>                   |
+|                                                          |
+\*________________________________________________________*/
+
+using System;
+using System.Threading.Tasks;
+using System.Web;
+using Hprose.RPC;
+
+namespace Hprose.RPC.AspNet {
+    public class AspNetHttpHandler : IHandler<HttpContext> {
+        public Task Bind(HttpContext server) {
+            throw new NotImplementedException();
+        }
+    }
+}
