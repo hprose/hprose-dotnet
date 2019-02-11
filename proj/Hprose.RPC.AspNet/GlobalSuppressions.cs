112c8515960ab4cdb762f9f0819d7ee5dd2f8a07
小马哥 <mabingyao@gmail.com>
2019-02-11 23:53:33 +0800

    Create GlobalSuppressions.cs

---
+++ proj/Hprose.RPC.AspNet/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "<挂起>", Scope = "member", Target = "~M:Hprose.RPC.AspNet.AspNetHttpHandler.GetOutputStream(System.Web.HttpRequest,System.Web.HttpResponse)~System.IO.Stream")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "<挂起>", Scope = "member", Target = "~M:Hprose.RPC.AspNet.AspNetHttpHandler.ClientAccessPolicyXmlHandler(System.Web.HttpRequest,System.Web.HttpResponse)~System.Threading.Tasks.Task{System.Boolean}")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "<挂起>", Scope = "member", Target = "~M:Hprose.RPC.AspNet.AspNetHttpHandler.CrossDomainXmlHandler(System.Web.HttpRequest,System.Web.HttpResponse)~System.Threading.Tasks.Task{System.Boolean}")]
