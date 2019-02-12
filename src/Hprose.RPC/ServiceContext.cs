3db9cd912a6f8ae689f354c278d11fadf901b6d3
小马哥 <mabingyao@gmail.com>
2019-02-12 16:51:07 +0800

    Update ServiceContext.cs

--- src/Hprose.RPC/ServiceContext.cs
+++ src/Hprose.RPC/ServiceContext.cs
@@ -21,10 +21,18 @@ namespace Hprose.RPC {
         public Service Service { get; private set; }
         public Method Method { get; set; } = null;
         public EndPoint RemoteEndPoint { get; set; } = null;
-        public dynamic RequestHeaders { get; } = new ExpandoObject();
-        public dynamic ResponseHeaders { get; } = new ExpandoObject();
+        public dynamic RequestHeaders { get; private set; } = new ExpandoObject();
+        public dynamic ResponseHeaders { get; private set; } = new ExpandoObject();
         public ServiceContext(Service service) {
             Service = service;
+        }
+        public override object Clone() {
+            ServiceContext context = base.Clone() as ServiceContext;
+            context.RequestHeaders = new ExpandoObject();
+            context.ResponseHeaders = new ExpandoObject();
+            Copy(RequestHeaders, context.RequestHeaders);
+            Copy(ResponseHeaders, context.ResponseHeaders);
+            return context;
         }
     }
 }
