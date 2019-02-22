c9a2e488d5279586ac813591dd46dd1a79f3d6c4
小马哥 <mabingyao@gmail.com>
2019-02-23 01:22:58 +0800

    Update WeightedLoadBalance.cs

--- src/Hprose.RPC.Plugins/LoadBalance/WeightedLoadBalance.cs
+++ src/Hprose.RPC.Plugins/LoadBalance/WeightedLoadBalance.cs
@@ -8,7 +8,7 @@
 |                                                          |
 |  Weighted LoadBalance plugin for C#.                     |
 |                                                          |
-|  LastModified: Feb 8, 2019                               |
+|  LastModified: Feb 23, 2019                              |
 |  Author: Ma Bingyao <andot@hprose.com>                   |
 |                                                          |
 \*________________________________________________________*/
@@ -20,18 +20,18 @@ using System.Threading.Tasks;
 
 namespace Hprose.RPC.Plugins.LoadBalance {
     public abstract class WeightedLoadBalance {
-        protected readonly string[] uris;
+        protected readonly Uri[] uris;
         protected readonly int[] weights;
         public WeightedLoadBalance(IDictionary<string, int> uriList) {
             if (uriList == null) {
                 throw new ArgumentNullException(nameof(uriList));
             }
             var n = uriList.Count;
-            uris = new string[n];
+            uris = new Uri[n];
             weights = new int[n];
             var i = 0;
             foreach (var pair in uriList) {
-                uris[i] = pair.Key;
+                uris[i] = new Uri(pair.Key);
                 weights[i] = pair.Value;
                 if (weights[i] <= 0) {
                     throw new ArgumentOutOfRangeException(nameof(uriList), "weight must be great than 0");
