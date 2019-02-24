bb489dc4228d9d3e27b70480a4e5737f0d6ae8d2
小马哥 <mabingyao@gmail.com>
2019-02-24 17:59:44 +0800

    Update ClientContext.cs

--- src/Hprose.RPC/ClientContext.cs
+++ src/Hprose.RPC/ClientContext.cs
@@ -8,7 +8,7 @@
 |                                                          |
 |  ClientContext class for C#.                             |
 |                                                          |
-|  LastModified: Feb 23, 2019                              |
+|  LastModified: Feb 24, 2019                              |
 |  Author: Ma Bingyao <andot@hprose.com>                   |
 |                                                          |
 \*________________________________________________________*/
@@ -20,17 +20,14 @@ namespace Hprose.RPC {
     public class ClientContext : Context {
         public Client Client { get; private set; }
         public Uri Uri { get; set; }
-        public Type Type { get; set; }
+        public Type ReturnType { get; set; }
         public IDictionary<string, object> RequestHeaders { get; private set; } = new Dictionary<string, object>(StringComparer.InvariantCultureIgnoreCase);
         public IDictionary<string, object> ResponseHeaders { get; private set; } = new Dictionary<string, object>(StringComparer.InvariantCultureIgnoreCase);
-        public ClientContext(Client client, Type type, Settings settings) {
+        public void Init(Client client, Type returnType) {
             Client = client;
             Uri = (client.Uris.Count > 0) ? client.Uris[0] : null;
-            Type = settings?.Type;
-            if (type != null && !type.IsAssignableFrom(Type)) Type = type;
+            ReturnType = returnType;
             Copy(client.RequestHeaders, RequestHeaders);
-            Copy(settings?.RequestHeaders, RequestHeaders);
-            Copy(settings?.Context, Items);
         }
         public override object Clone() {
             var context = base.Clone() as ClientContext;
