4165d9bedd53bd50ae29b4e30605cc06a9c6d0f7
小马哥 <mabingyao@gmail.com>
2019-02-25 20:35:23 +0800

    Update WebSocketTransport.cs

--- src/Hprose.RPC/WebSocketTransport.cs
+++ src/Hprose.RPC/WebSocketTransport.cs
@@ -8,7 +8,7 @@
 |                                                          |
 |  WebSocketTransport class for C#.                        |
 |                                                          |
-|  LastModified: Feb 23, 2019                              |
+|  LastModified: Feb 25, 2019                              |
 |  Author: Ma Bingyao <andot@hprose.com>                   |
 |                                                          |
 \*________________________________________________________*/
@@ -36,16 +36,17 @@ namespace Hprose.RPC {
         public bool UseDefaultCredentials { get; set; } = true;
         public int ReceiveBufferSize { get; set; } = 16384;
         public int SendBufferSize { get; set; } = 16384;
-        private ConcurrentDictionary<Uri, ConcurrentQueue<(int, MemoryStream)>> Requests { get; } = new ConcurrentDictionary<Uri, ConcurrentQueue<(int, MemoryStream)>>();
-        private ConcurrentDictionary<Uri, ConcurrentDictionary<int, TaskCompletionSource<MemoryStream>>> Results { get; } = new ConcurrentDictionary<Uri, ConcurrentDictionary<int, TaskCompletionSource<MemoryStream>>>();
-        private ConcurrentDictionary<Uri, ConcurrentDictionary<int, TaskCompletionSource<MemoryStream>>> Sended { get; } = new ConcurrentDictionary<Uri, ConcurrentDictionary<int, TaskCompletionSource<MemoryStream>>>();
+        private ConcurrentDictionary<ClientWebSocket, ConcurrentQueue<(int, MemoryStream)>> Requests { get; } = new ConcurrentDictionary<ClientWebSocket, ConcurrentQueue<(int, MemoryStream)>>();
+        private ConcurrentDictionary<ClientWebSocket, ConcurrentDictionary<int, TaskCompletionSource<MemoryStream>>> Results { get; } = new ConcurrentDictionary<ClientWebSocket, ConcurrentDictionary<int, TaskCompletionSource<MemoryStream>>>();
+        private ConcurrentDictionary<ClientWebSocket, byte> Lock { get; } = new ConcurrentDictionary<ClientWebSocket, byte>();
         private ConcurrentDictionary<Uri, Lazy<Task<ClientWebSocket>>> WebSockets { get; } = new ConcurrentDictionary<Uri, Lazy<Task<ClientWebSocket>>>();
         private readonly Func<Uri, Lazy<Task<ClientWebSocket>>> webSocketFactory;
         public WebSocketTransport() {
             webSocketFactory = (uri) => {
                 return new Lazy<Task<ClientWebSocket>>(async () => {
+                    ClientWebSocket webSocket = null;
                     try {
-                        var webSocket = new ClientWebSocket();
+                        webSocket = new ClientWebSocket();
                         var options = webSocket.Options;
                         options.AddSubProtocol("hprose");
                         options.UseDefaultCredentials = UseDefaultCredentials;
@@ -66,39 +67,39 @@ namespace Hprose.RPC {
                             options.KeepAliveInterval = KeepAliveInterval;
                         }
                         await webSocket.ConnectAsync(uri, CancellationToken.None);
-                        Receive(uri, webSocket);
+                        Requests.TryAdd(webSocket, new ConcurrentQueue<(int, MemoryStream)>());
+                        Results.TryAdd(webSocket, new ConcurrentDictionary<int, TaskCompletionSource<MemoryStream>>());
+                        Receive(webSocket);
                         return webSocket;
                     }
                     catch (Exception e) {
-                        var requests = Requests[uri];
-                        var sended = Sended[uri];
-                        var results = Results[uri];
-                        if (requests != null) {
-                            while (!requests.IsEmpty) {
-                                if (requests.TryDequeue(out (int index, MemoryStream stream) request)) {
-                                    if (sended.TryRemove(request.index, out var result)) {
-                                        result.TrySetException(e);
-                                    }
-                                    if (results.TryRemove(request.index, out result)) {
-                                        result.TrySetException(e);
-                                    }
-                                }
-                            }
+                        if (webSocket != null) {
+                            Close(webSocket, e);
                         }
                         throw;
                     }
                 });
             };
         }
-        public async void Abort() {
-            foreach (var LazyWebSocket in WebSockets.Values) {
-                try {
-                    var webSocket = await LazyWebSocket.Value.ConfigureAwait(false);
-                    webSocket.Abort();
-                    webSocket.Dispose();
+        private void Close(ClientWebSocket webSocket, Exception e) {
+            try {
+                if (e.InnerException != null) {
+                    e = e.InnerException;
                 }
-                catch { }
+                Requests.TryRemove(webSocket, out var requests);
+                if (Results.TryRemove(webSocket, out var results)) {
+                    while (!results.IsEmpty) {
+                        foreach (var i in results.Keys) {
+                            if (results.TryRemove(i, out var result)) {
+                                result.TrySetException(e);
+                            }
+                        }
+                    }
+                }
+                webSocket.Abort();
+                webSocket.Dispose();
             }
+            catch { }
         }
         private async Task<(int, MemoryStream)> ReadAsync(ClientWebSocket webSocket) {
             MemoryStream stream = new MemoryStream();
@@ -127,14 +128,14 @@ namespace Hprose.RPC {
                 ArrayPool<byte>.Shared.Return(buffer);
             }
         }
-        private async void Receive(Uri uri, ClientWebSocket webSocket) {
-            var sended = Sended[uri];
+        private async void Receive(ClientWebSocket webSocket) {
+            var results = Results[webSocket];
             try {
                 while (true) {
                     var (index, response) = await ReadAsync(webSocket).ConfigureAwait(false);
                     bool has_error = (index & 0x80000000) != 0;
                     index &= 0x7FFFFFFF;
-                    if (sended.TryRemove(index, out var result)) {
+                    if (results.TryRemove(index, out var result)) {
                         if (has_error) {
                             var buffer = response.GetArraySegment();
                             result.TrySetException(new Exception(Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count)));
@@ -145,68 +146,41 @@ namespace Hprose.RPC {
                 }
             }
             catch (Exception e) {
-                foreach (var index in sended.Keys) {
-                    if (sended.TryRemove(index, out var result)) {
-                        result.TrySetException(e);
-                    }
-                }
-                webSocket.Abort();
-                webSocket.Dispose();
+                Close(webSocket, e);
             }
         }
-        private async Task Send(Uri uri, int index, MemoryStream stream) {
-            int n = (int)stream.Length;
-            var buffer = ArrayPool<byte>.Shared.Rent(4 + n);
-            ClientWebSocket webSocket = null;
+        private async void Send(ClientWebSocket webSocket) {
+            if (!Lock.TryAdd(webSocket, default)) return;
             try {
-                webSocket = await GetWebSocket(uri).ConfigureAwait(false);
-                buffer[0] = (byte)(index >> 24 & 0xFF);
-                buffer[1] = (byte)(index >> 16 & 0xFF);
-                buffer[2] = (byte)(index >> 8 & 0xFF);
-                buffer[3] = (byte)(index & 0xFF);
-                stream.Read(buffer, 4, n);
-                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, 4 + n), WebSocketMessageType.Binary, true, CancellationToken.None).ConfigureAwait(false);
-            }
-            catch (Exception e) {
-                var sended = Sended[uri];
-                foreach (var i in sended.Keys) {
-                    if (sended.TryRemove(i, out var value)) {
-                        value.TrySetException(e);
+                var requests = Requests[webSocket];
+                while (requests.TryDequeue(out var request)) {
+                    var (index, stream) = request;
+                    int n = (int)stream.Length;
+                    var buffer = ArrayPool<byte>.Shared.Rent(4 + n);
+                    try {
+                        buffer[0] = (byte)(index >> 24 & 0xFF);
+                        buffer[1] = (byte)(index >> 16 & 0xFF);
+                        buffer[2] = (byte)(index >> 8 & 0xFF);
+                        buffer[3] = (byte)(index & 0xFF);
+                        stream.Read(buffer, 4, n);
+                        await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, 4 + n), WebSocketMessageType.Binary, true, CancellationToken.None).ConfigureAwait(false);
                     }
-                }
-                if (webSocket != null) {
-                    webSocket.Abort();
-                    webSocket.Dispose();
+                    catch (Exception e) {
+                        Close(webSocket, e);
+                    }
+                    finally {
+                        stream.Dispose();
+                        ArrayPool<byte>.Shared.Return(buffer);
+                    }
                 }
             }
             finally {
-                stream.Dispose();
-                ArrayPool<byte>.Shared.Return(buffer);
-            }
-        }
-        private async void Send(Uri uri) {
-            var requests = Requests[uri];
-            var results = Results[uri];
-            var sended = Sended.GetOrAdd(uri, (_) => new ConcurrentDictionary<int, TaskCompletionSource<MemoryStream>>());
-            while (!requests.IsEmpty) {
-                requests.TryPeek(out (int index, MemoryStream stream) request);
-                var index = request.index;
-                if (results.TryGetValue(index, out var result)) {
-                    if (!(sended.TryAdd(index, result) && results.TryRemove(index, out result))) {
-                        await Task.Yield();
-                        continue;
-                    }
-                }
-                else {
-                    return;
-                }
-                await Send(uri, index, request.stream).ConfigureAwait(false);
-                requests.TryDequeue(out request);
+                Lock.TryRemove(webSocket, out var _);
             }
         }
         private async Task<ClientWebSocket> GetWebSocket(Uri uri) {
             int retried = 0;
-            while(true) {
+            while (true) {
                 var LazyWebSocket = WebSockets.GetOrAdd(uri, webSocketFactory);
                 var webSocket = await LazyWebSocket.Value.ConfigureAwait(false);
                 if (webSocket.CloseStatus == null && webSocket.State == WebSocketState.Open) {
@@ -224,16 +198,24 @@ namespace Hprose.RPC {
             var stream = await request.ToMemoryStream().ConfigureAwait(false);
             var uri = (context as ClientContext).Uri;
             var index = Interlocked.Increment(ref counter) & 0x7FFFFFFF;
-            var results = Results.GetOrAdd(uri, (_) => new ConcurrentDictionary<int, TaskCompletionSource<MemoryStream>>());
+            var webSocket = await GetWebSocket(uri).ConfigureAwait(false);
+            var results = Results[webSocket];
             var result = new TaskCompletionSource<MemoryStream>();
             results[index] = result;
-            var requests = Requests.GetOrAdd(uri, (_) => new ConcurrentQueue<(int, MemoryStream)>());
+            var requests = Requests[webSocket];
             requests.Enqueue((index, stream));
-            requests.TryPeek(out (int index, MemoryStream stream) first);
-            if (first.index == index) {
-                Send(uri);
-            }
+            Send(webSocket);
             return await result.Task.ConfigureAwait(false);
+        }
+        public async void Abort() {
+            foreach (var LazyWebSocket in WebSockets.Values) {
+                try {
+                    var webSocket = await LazyWebSocket.Value.ConfigureAwait(false);
+                    webSocket.Abort();
+                    webSocket.Dispose();
+                }
+                catch { }
+            }
         }
     }
 }
