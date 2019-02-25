16e5ebeb38f17b269eb41bf05558e9d821ff1e38
小马哥 <mabingyao@gmail.com>
2019-02-25 20:35:12 +0800

    Update TcpTransport.cs

--- src/Hprose.RPC/TcpTransport.cs
+++ src/Hprose.RPC/TcpTransport.cs
@@ -8,7 +8,7 @@
 |                                                          |
 |  TcpTransport class for C#.                              |
 |                                                          |
-|  LastModified: Feb 23, 2019                              |
+|  LastModified: Feb 25, 2019                              |
 |  Author: Ma Bingyao <andot@hprose.com>                   |
 |                                                          |
 \*________________________________________________________*/
@@ -44,9 +44,9 @@ namespace Hprose.RPC {
         public EncryptionPolicy EncryptionPolicy { get; set; } = EncryptionPolicy.RequireEncryption;
         public string ServerCertificateName { get; set; } = null;
 #endif
-        private ConcurrentDictionary<Uri, ConcurrentQueue<(int, MemoryStream)>> Requests { get; } = new ConcurrentDictionary<Uri, ConcurrentQueue<(int, MemoryStream)>>();
-        private ConcurrentDictionary<Uri, ConcurrentDictionary<int, TaskCompletionSource<MemoryStream>>> Results { get; } = new ConcurrentDictionary<Uri, ConcurrentDictionary<int, TaskCompletionSource<MemoryStream>>>();
-        private ConcurrentDictionary<Uri, ConcurrentDictionary<int, TaskCompletionSource<MemoryStream>>> Sended { get; } = new ConcurrentDictionary<Uri, ConcurrentDictionary<int, TaskCompletionSource<MemoryStream>>>();
+        private ConcurrentDictionary<TcpClient, ConcurrentQueue<(int, MemoryStream)>> Requests { get; } = new ConcurrentDictionary<TcpClient, ConcurrentQueue<(int, MemoryStream)>>();
+        private ConcurrentDictionary<TcpClient, ConcurrentDictionary<int, TaskCompletionSource<MemoryStream>>> Results { get; } = new ConcurrentDictionary<TcpClient, ConcurrentDictionary<int, TaskCompletionSource<MemoryStream>>>();
+        private ConcurrentDictionary<TcpClient, byte> Lock { get; } = new ConcurrentDictionary<TcpClient, byte>();
         private ConcurrentDictionary<Uri, Lazy<Task<(TcpClient, Stream)>>> TcpClients { get; } = new ConcurrentDictionary<Uri, Lazy<Task<(TcpClient, Stream)>>>();
 #if !NET35_CF
         private readonly Func<Uri, Lazy<Task<(TcpClient, Stream)>>> tcpClientFactory;
@@ -56,6 +56,7 @@ namespace Hprose.RPC {
         public TcpTransport() {
             tcpClientFactory = (uri) => {
                 return new Lazy<Task<(TcpClient, Stream)>>(async () => {
+                    TcpClient tcpClient = null;
                     try {
                         var family = uri.Scheme.Last() == '6' ? AddressFamily.InterNetworkV6 : AddressFamily.InterNetwork;
 #if NET35_CF || NET40 || NET45 || NET451 || NET452
@@ -64,17 +65,17 @@ namespace Hprose.RPC {
                         var host = uri.IdnHost;
 #endif
                         var port = uri.Port > 0 ? uri.Port : 8412;
-                        var client = new TcpClient(family) {
+                        tcpClient = new TcpClient(family) {
                             NoDelay = NoDelay,
                             ReceiveBufferSize = ReceiveBufferSize,
                             SendBufferSize = SendBufferSize
                         };
                         if (LingerState != null) {
-                            client.LingerState = LingerState;
+                            tcpClient.LingerState = LingerState;
                         }
 
-                        await client.ConnectAsync(host, port);
-                        Stream stream = client.GetStream();
+                        await tcpClient.ConnectAsync(host, port);
+                        Stream tcpStream = tcpClient.GetStream();
 #if !NET35_CF
                         switch (uri.Scheme) {
                             case "tls":
@@ -83,47 +84,44 @@ namespace Hprose.RPC {
                             case "ssl":
                             case "ssl4":
                             case "ssl6":
-                                SslStream sslstream = new SslStream(stream, false, ValidateServerCertificate, CertificateSelection, EncryptionPolicy);
+                                SslStream sslstream = new SslStream(tcpStream, false, ValidateServerCertificate, CertificateSelection, EncryptionPolicy);
                                 await sslstream.AuthenticateAsClientAsync(ServerCertificateName ?? uri.Host).ConfigureAwait(false);
-                                stream = sslstream;
+                                tcpStream = sslstream;
                                 break;
                         }
 #endif
-                        Receive(uri, client, stream);
-                        return (client, stream);
+                        Requests.TryAdd(tcpClient, new ConcurrentQueue<(int, MemoryStream)>());
+                        Results.TryAdd(tcpClient, new ConcurrentDictionary<int, TaskCompletionSource<MemoryStream>>());
+                        Receive(tcpClient, tcpStream);
+                        return (tcpClient, tcpStream);
                     }
                     catch (Exception e) {
-                        if (e.InnerException != null) {
-                            e = e.InnerException;
-                        }
-                        var requests = Requests[uri];
-                        var sended = Sended[uri];
-                        var results = Results[uri];
-                        if (requests != null) {
-                            while (!requests.IsEmpty) {
-                                if (requests.TryDequeue(out (int index, MemoryStream stream) request)) {
-                                    if (sended.TryRemove(request.index, out var result)) {
-                                        result.TrySetException(e);
-                                    }
-                                    if (results.TryRemove(request.index, out result)) {
-                                        result.TrySetException(e);
-                                    }
-                                }
-                            }
+                        if (tcpClient != null) {
+                            Close(tcpClient, e);
                         }
                         throw;
                     }
                 });
             };
         }
-        public async void Abort() {
-            foreach (var LazyTcpClient in TcpClients.Values) {
-                try {
-                    var (tcpClient, _) = await LazyTcpClient.Value.ConfigureAwait(false);
-                    tcpClient.Close();
+        private void Close(TcpClient tcpClient, Exception e) {
+            try {
+                if (e.InnerException != null) {
+                    e = e.InnerException;
                 }
-                catch { }
+                Requests.TryRemove(tcpClient, out var requests);
+                if (Results.TryRemove(tcpClient, out var results)) {
+                    while (!results.IsEmpty) {
+                        foreach (var i in results.Keys) {
+                            if (results.TryRemove(i, out var result)) {
+                                result.TrySetException(e);
+                            }
+                        }
+                    }
+                }
+                tcpClient.Close();
             }
+            catch { }
         }
         private static async Task<byte[]> ReadAsync(Stream stream, byte[] bytes, int offset, int length) {
             while (length > 0) {
@@ -133,12 +131,12 @@ namespace Hprose.RPC {
             }
             return bytes;
         }
-        private async void Receive(Uri uri, TcpClient tcpClient, Stream netStream) {
-            var sended = Sended[uri];
+        private async void Receive(TcpClient tcpClient, Stream tcpStream) {
+            var results = Results[tcpClient];
             var header = new byte[12];
             try {
                 while (true) {
-                    await ReadAsync(netStream, header, 0, 12).ConfigureAwait(false);
+                    await ReadAsync(tcpStream, header, 0, 12).ConfigureAwait(false);
                     uint crc = (uint)((header[0] << 24) | (header[1] << 16) | (header[2] << 8) | header[3]);
                     if (CRC32.Compute(header, 4, 8) != crc || (header[4] & 0x80) == 0) {
                         throw new IOException("invalid response");
@@ -147,8 +145,8 @@ namespace Hprose.RPC {
                     int index = (header[8] << 24) | (header[9] << 16) | (header[10] << 8) | header[11];
                     bool has_error = (index & 0x80000000) != 0;
                     index &= 0x7FFFFFFF;
-                    var data = await ReadAsync(netStream, new byte[length], 0, length).ConfigureAwait(false);
-                    if (sended.TryRemove(index, out var result)) {
+                    var data = await ReadAsync(tcpStream, new byte[length], 0, length).ConfigureAwait(false);
+                    if (results.TryRemove(index, out var result)) {
                         if (has_error) {
                             result.TrySetException(new Exception(Encoding.UTF8.GetString(data, 0, data.Length)));
                             throw new IOException("connection closed");
@@ -158,93 +156,63 @@ namespace Hprose.RPC {
                 }
             }
             catch (Exception e) {
-                foreach (var index in sended.Keys) {
-                    if (sended.TryRemove(index, out var result)) {
-                        result.TrySetException(e);
-                    }
-                }
-                tcpClient.Close();
+                Close(tcpClient, e);
             }
         }
-        private async Task Send(Uri uri, int index, MemoryStream stream) {
-            var header = new byte[12];
-            var n = (int)stream.Length;
-            header[4] = (byte)(n >> 24 & 0xFF | 0x80);
-            header[5] = (byte)(n >> 16 & 0xFF);
-            header[6] = (byte)(n >> 8 & 0xFF);
-            header[7] = (byte)(n & 0xFF);
-            header[8] = (byte)(index >> 24 & 0xFF);
-            header[9] = (byte)(index >> 16 & 0xFF);
-            header[10] = (byte)(index >> 8 & 0xFF);
-            header[11] = (byte)(index & 0xFF);
-            var crc32 = CRC32.Compute(header, 4, 8);
-            header[0] = (byte)(crc32 >> 24 & 0xFF);
-            header[1] = (byte)(crc32 >> 16 & 0xFF);
-            header[2] = (byte)(crc32 >> 8 & 0xFF);
-            header[3] = (byte)(crc32 & 0xFF);
-            TcpClient tcpClient = null;
-            Stream netStream = null;
+        private async void Send(TcpClient tcpClient, Stream tcpStream) {
+            if (!Lock.TryAdd(tcpClient, default)) return;
             try {
-                (tcpClient, netStream) = await GetTcpClient(uri).ConfigureAwait(false);
-                await netStream.WriteAsync(header, 0, 12).ConfigureAwait(false);
-                await stream.CopyToAsync(netStream).ConfigureAwait(false);
-                await netStream.FlushAsync().ConfigureAwait(false);
-            }
-            catch (Exception e) {
-                var sended = Sended[uri];
-                foreach (var i in sended.Keys) {
-                    if (sended.TryRemove(i, out var value)) {
-                        value.TrySetException(e);
+                var requests = Requests[tcpClient];
+                var header = new byte[12];
+                while (requests.TryDequeue(out var request)) {
+                    var (index, stream) = request;
+                    var n = (int)stream.Length;
+                    header[4] = (byte)(n >> 24 & 0xFF | 0x80);
+                    header[5] = (byte)(n >> 16 & 0xFF);
+                    header[6] = (byte)(n >> 8 & 0xFF);
+                    header[7] = (byte)(n & 0xFF);
+                    header[8] = (byte)(index >> 24 & 0xFF);
+                    header[9] = (byte)(index >> 16 & 0xFF);
+                    header[10] = (byte)(index >> 8 & 0xFF);
+                    header[11] = (byte)(index & 0xFF);
+                    var crc32 = CRC32.Compute(header, 4, 8);
+                    header[0] = (byte)(crc32 >> 24 & 0xFF);
+                    header[1] = (byte)(crc32 >> 16 & 0xFF);
+                    header[2] = (byte)(crc32 >> 8 & 0xFF);
+                    header[3] = (byte)(crc32 & 0xFF);
+                    try {
+                        await tcpStream.WriteAsync(header, 0, 12).ConfigureAwait(false);
+                        await stream.CopyToAsync(tcpStream).ConfigureAwait(false);
+                        await tcpStream.FlushAsync().ConfigureAwait(false);
                     }
-                }
-                if (tcpClient != null) {
-                    tcpClient.Close();
+                    catch (Exception e) {
+                        Close(tcpClient, e);
+                    }
+                    finally {
+                        stream.Dispose();
+                    }
                 }
             }
             finally {
-                stream.Dispose();
-            }
-        }
-        private async void Send(Uri uri) {
-            var requests = Requests[uri];
-            var results = Results[uri];
-            var sended = Sended.GetOrAdd(uri, (_) => new ConcurrentDictionary<int, TaskCompletionSource<MemoryStream>>());
-            while (!requests.IsEmpty) {
-                requests.TryPeek(out (int index, MemoryStream stream) request);
-                var index = request.index;
-                if (results.TryGetValue(index, out var result)) {
-                    if (!(sended.TryAdd(index, result) && results.TryRemove(index, out result))) {
-#if NET40
-                        await TaskEx.Yield();
-#else
-                        await Task.Yield();
-#endif
-                        continue;
-                    }
-                }
-                else {
-                    return;
-                }
-                await Send(uri, index, request.stream).ConfigureAwait(false);
-                requests.TryDequeue(out request);
+                Lock.TryRemove(tcpClient, out var _);
             }
         }
         private async Task<(TcpClient, Stream)> GetTcpClient(Uri uri) {
             int retried = 0;
             while(true) {
                 var LazyTcpClient = TcpClients.GetOrAdd(uri, tcpClientFactory);
-                var (tcpClient, stream) = await LazyTcpClient.Value.ConfigureAwait(false);
+                var (tcpClient, tcpStream) = await LazyTcpClient.Value.ConfigureAwait(false);
                 try {
 #if !NET35_CF
                     var available = tcpClient.Available;
 #else
                     var available = tcpClient.Client.Available;
 #endif
-                    return (tcpClient, stream);
+                    return (tcpClient, tcpStream);
                 }
-                catch {
+                catch (Exception e) {
                     TcpClients.TryRemove(uri, out LazyTcpClient);
-                    tcpClient.Close();
+                    Close(tcpClient, e);
                     if (++retried > 1) {
                         throw;
                     }
@@ -256,16 +224,21 @@ namespace Hprose.RPC {
             var clientContext = context as ClientContext;
             var uri = clientContext.Uri;
             var index = Interlocked.Increment(ref counter) & 0x7FFFFFFF;
-            var results = Results.GetOrAdd(uri, (_) => new ConcurrentDictionary<int, TaskCompletionSource<MemoryStream>>());
+            var (tcpClient, tcpStream) = await GetTcpClient(uri).ConfigureAwait(false);
             var result = new TaskCompletionSource<MemoryStream>();
-            results[index] = result;
-            var requests = Requests.GetOrAdd(uri, (_) => new ConcurrentQueue<(int, MemoryStream)>());
-            requests.Enqueue((index, stream));
-            requests.TryPeek(out (int index, MemoryStream stream) first);
-            if (first.index == index) {
-                Send(uri);
-            }
+            Results[tcpClient][index] = result;
+            Requests[tcpClient].Enqueue((index, stream));
+            Send(tcpClient, tcpStream);
             return await result.Task.ConfigureAwait(false);
+        }
+        public async void Abort() {
+            foreach (var LazyTcpClient in TcpClients.Values) {
+                try {
+                    var (tcpClient, _) = await LazyTcpClient.Value.ConfigureAwait(false);
+                    tcpClient.Close();
+                }
+                catch { }
+            }
         }
     }
 }
