e6c307aa02916d01e9c2692ea83a50236c0e3aaf
小马哥 <mabingyao@gmail.com>
2019-03-20 02:29:57 +0800

    Update TaskResult.cs

--- src/Hprose.RPC/TaskResult.cs
+++ src/Hprose.RPC/TaskResult.cs
@@ -8,7 +8,7 @@
 |                                                          |
 |  TaskResult class for C#.                                |
 |                                                          |
-|  LastModified: Feb 21, 2019                              |
+|  LastModified: Mar 20, 2019                              |
 |  Author: Ma Bingyao <andot@hprose.com>                   |
 |                                                          |
 \*________________________________________________________*/
@@ -24,19 +24,20 @@ namespace Hprose.RPC {
         private static readonly ConcurrentDictionary<Type, Lazy<Func<Task, Task<object>>>> cache = new ConcurrentDictionary<Type, Lazy<Func<Task, Task<object>>>>();
         private static Func<Task, Task<object>> GetFunc(Type type) {
             var resultType = type.GetGenericArguments()[0];
-            var method = typeof(TaskResult).GetMethod("Get", BindingFlags.NonPublic | BindingFlags.Static).MakeGenericMethod(resultType);
+            var method = typeof(TaskResult).GetMethod(nameof(GetTask), BindingFlags.NonPublic | BindingFlags.Static).MakeGenericMethod(resultType);
             var task = Expression.Parameter(typeof(Task), "task");
             return Expression.Lambda<Func<Task, Task<object>>>(
                 Expression.Call(method, Expression.Convert(task, type)),
                 task
             ).Compile();
         }
-        static async Task<object> Get<T>(Task<T> task) => await task.ConfigureAwait(false);
+        static async Task<object> GetTask<T>(Task<T> task) => await task.ConfigureAwait(false);
 #if !NET35_CF
         private static readonly Func<Type, Lazy<Func<Task, Task<object>>>> factory = (type) => new Lazy<Func<Task, Task<object>>>(() => GetFunc(type));
 #else
         private static readonly Func2<Type, Lazy<Func<Task, Task<object>>>> factory = (type) => new Lazy<Func<Task, Task<object>>>(() => GetFunc(type));
 #endif
+        public static async Task<object> Get<T>(Task<T> task) => await task.ConfigureAwait(false);
         public static async Task<object> Get(Task task) {
             var type = task.GetType();
             if (type.IsGenericType) {
