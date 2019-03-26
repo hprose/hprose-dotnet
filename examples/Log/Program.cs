c48f7322ea673698987dab735ee9137ede55d465
小马哥 <mabingyao@gmail.com>
2019-03-27 00:48:24 +0800

    Added two examples.

---
+++ examples/Log/Program.cs
@@ -0,0 +1,44 @@
+﻿using Hprose.RPC;
+using Hprose.RPC.Plugins.Log;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading.Tasks;
+
+class MyService {
+    public int Sum(int x, int y) {
+        return x + y;
+    }
+    public string Hello(string name, ServiceContext context) {
+        var endPoint = context.RemoteEndPoint as IPEndPoint;
+        return "Hello " + name + " from " + endPoint.Address + ":" + endPoint.Port;
+    }
+}
+
+public interface IMyService {
+    [Log(false)]
+    Task<int> Sum(int x, int y);
+    Task<string> Hello(string name);
+}
+
+class Program {
+    static async Task RunClient() {
+        var client = new Client("http://127.0.0.1:8080/");
+        client.Use(Log.InvokeHandler);
+        var proxy = client.UseService<IMyService>();
+        Console.WriteLine(await proxy.Hello("world"));
+        Console.WriteLine(await proxy.Sum(1, 2));
+    }
+    static void Main(string[] args) {
+        Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+        HttpListener server = new HttpListener();
+        server.Prefixes.Add("http://127.0.0.1:8080/");
+        server.Start();
+        var service = new Service();
+        service.Use(Log.IOHandler)
+            .AddInstanceMethods(new MyService())
+            .Bind(server);
+        RunClient().Wait();
+        server.Stop();
+    }
+}
---
+++ examples/Oneway/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Hprose.RPC;
+using Hprose.RPC.Plugins.Oneway;
+
+public interface IRestart {
+    [Oneway]
+    Task Restart();
+}
+class Program {
+    // static void Main(string[] args) {
+    //     var client = new Client("http://127.0.0.1/");
+    //     client.Use(Oneway.Handler);
+    //     client.Invoke("restart", null, new ClientContext() {
+    //         ["Oneway"] = true
+    //     });
+    // }
+
+    static async Task Example() {
+        var client = new Client("http://127.0.0.1/");
+        client.Use(Oneway.Handler);
+        var proxy = client.UseService<IRestart>();
+        await proxy.Restart();
+    }
+    static void Main(string[] args) {
+        Example().Wait();
+    }
+}
