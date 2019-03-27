f237a72cc4016ed2b22c9dd7e1db18f1d284b620
小马哥 <mabingyao@gmail.com>
2019-03-27 20:19:04 +0800

    Update Program.cs

--- examples/Limiter/Program.cs
+++ examples/Limiter/Program.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Net;
-using System.Net.Sockets;
-using System.Threading.Tasks;
-using Hprose.RPC;
+﻿using Hprose.RPC;
 using Hprose.RPC.Plugins.Limiter;
+using System;
+using System.Net;
+using System.Threading.Tasks;
 
 class MyService {
     public int Sum(int x, int y) {
@@ -17,8 +16,8 @@ public interface IMyService {
 
 class Program {
     static async Task RunClient() {
-        var client = new Client("tcp4://127.0.0.1:8412");
-        client.Use(new ConcurrentLimiter(64).Handler).Use(new RateLimiter(10000).InvokeHandler);
+        var client = new Client("http://127.0.0.1:8080/");
+        client.Use(new ConcurrentLimiter(64).Handler).Use(new RateLimiter(2000).InvokeHandler);
         var begin = DateTime.Now;
         var proxy = client.UseService<IMyService>();
         var n = 5000;
@@ -31,8 +30,8 @@ class Program {
         Console.WriteLine(end - begin);
     }
     static void Main(string[] args) {
-        IPAddress iPAddress = Dns.GetHostAddresses("127.0.0.1")[0];
-        TcpListener server = new TcpListener(iPAddress, 8412);
+        HttpListener server = new HttpListener();
+        server.Prefixes.Add("http://127.0.0.1:8080/");
         server.Start();
         var service = new Service();
         service.AddInstanceMethods(new MyService()).Bind(server);
