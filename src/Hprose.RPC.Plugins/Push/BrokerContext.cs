0ad83ac4db1a95b4a5bf7512ed29300317415590
小马哥 <mabingyao@gmail.com>
2020-03-29 02:08:29 +0800

    Create BrokerContext.cs

---
+++ src/Hprose.RPC.Plugins/Push/BrokerContext.cs
@@ -0,0 +1,24 @@
+﻿/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+|  BrokerContext.cs                                        |
+|                                                          |
+|  BrokerContext class for C#.                             |
+|                                                          |
+|  LastModified: Mar 29, 2020                              |
+|  Author: Ma Bingyao <andot@hprose.com>                   |
+|                                                          |
+\*________________________________________________________*/
+
+namespace Hprose.RPC.Plugins.Push {
+    public class BrokerContext : ServiceContext {
+        public IProducer Producer { get; private set; }
+        public BrokerContext(IProducer producer, ServiceContext context) : base(context.Service) {
+            context.CopyTo(this);
+            Producer = producer;
+        }
+    }
+}
